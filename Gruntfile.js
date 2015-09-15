bbf6276b60c3ffd4dfdd4f7140fa9dafebb30b40
Sylvain UTARD <sylvain.utard@gmail.com>
2015-09-16 00:15:11 +0200

    Add banned in the minified versions as well

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,6 +16,7 @@ module.exports = function(grunt) {
 
     uglify: {
       options: {
+        banner: '<%= banner %>'
       },
       jquery: {
         src: '<%= buildDir %>/autocomplete.jquery.js',
