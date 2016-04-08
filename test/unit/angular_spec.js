a7be7103abf79a10252515da2aeb61aa645776b0
Sylvain UTARD <sylvain.utard@gmail.com>
2016-04-08 23:09:04 +0200

    tests: bind polyfill as described in https://github.com/angular/angular.js/issues/13794

--- test/unit/angular_spec.js
+++ test/unit/angular_spec.js
@@ -2,6 +2,24 @@
 
 /* eslint-env mocha, jasmine */
 
+if (typeof Function.prototype.bind != 'function') {
+  Function.prototype.bind = function bind(obj) {
+    var args = Array.prototype.slice.call(arguments, 1),
+      self = this,
+      nop = function() {
+      },
+      bound = function() {
+        return self.apply(
+          this instanceof nop ? this : (obj || {}), args.concat(
+            Array.prototype.slice.call(arguments)
+          )
+        );
+      };
+    nop.prototype = this.prototype || {};
+    bound.prototype = new nop();
+    return bound;
+  };
+}
 
 describe('autocomplete directive', function() {
   global.jQuery = require('jquery');
