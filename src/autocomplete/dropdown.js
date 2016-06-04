rickybell/autocomplete.js

Render an `empty` template if no results
Fix #79

src/autocomplete/dropdown.js
@@ -31,6 +31,7 @@ function Dropdown(o) {
   this.isOpen = false;
   this.isEmpty = true;
   this.cssClasses = _.mixin({}, css.defaultClasses, o.cssClasses || {});
+  this.templates = {};
 
   // bound functions
   onSuggestionClick = _.bind(this._onSuggestionClick, this);
@@ -44,10 +45,13 @@ function Dropdown(o) {
     .on('mouseleave.aa', cssClass, onSuggestionMouseLeave);
 
   if (o.templates && o.templates.header) {
-    this.$menu.prepend(_.templatify(o.templates.header)());
+    this.templates.header = _.templatify(o.templates.header);
+    this.$menu.prepend(this.templates.header());
   }
 
-  this.datasets = _.map(o.datasets, function(oDataset) { return initializeDataset(that.$menu, oDataset, o.cssClasses); });
+  this.datasets = _.map(o.datasets, function(oDataset) {
+    return initializeDataset(that.$menu, oDataset, o.cssClasses);
+  });
   _.each(this.datasets, function(dataset) {
     var root = dataset.getRoot();
     if (root && root.parent().length === 0) {
@@ -57,7 +61,16 @@ function Dropdown(o) {
   });
 
   if (o.templates && o.templates.footer) {
-    this.$menu.append(_.templatify(o.templates.footer)());
+    this.templates.header = _.templatify(o.templates.footer);
+    this.$menu.append(this.templates.footer());
+  }
+
+  if (o.templates && o.templates.empty) {
+    this.templates.empty = _.templatify(o.templates.empty);
+    this.$empty = DOM.element('<div class="' +
+      _.className(this.cssClasses.prefix, this.cssClasses.empty, true) + '">' +
+      '</div>');
+    this.$menu.append(this.$empty);
   }
 }
 
@@ -101,8 +114,18 @@ _.mixin(Dropdown.prototype, EventEmitter, {
     this.isEmpty = _.every(this.datasets, isDatasetEmpty);
 
     if (this.isEmpty) {
-      this._hide();
+      if (this.$empty) {
+        var html = this.templates.empty({
+          query: this.datasets[0] && this.datasets[0].query
+        });
+        this.$empty.html(html);
+      } else {
+        this._hide();
+      }
     } else if (this.isOpen) {
+      if (this.$empty) {
+        this.$empty.empty();
+      }
       this._show();
     }
 

algolia/autocomplete

src/autocomplete/dropdown.js
@@ -31,6 +31,7 @@ function Dropdown(o) {
   this.isOpen = false;
   this.isEmpty = true;
   this.cssClasses = _.mixin({}, css.defaultClasses, o.cssClasses || {});
+  this.templates = {};
 
   // bound functions
   onSuggestionClick = _.bind(this._onSuggestionClick, this);
@@ -44,10 +45,13 @@ function Dropdown(o) {
     .on('mouseleave.aa', cssClass, onSuggestionMouseLeave);
 
   if (o.templates && o.templates.header) {
-    this.$menu.prepend(_.templatify(o.templates.header)());
+    this.templates.header = _.templatify(o.templates.header);
+    this.$menu.prepend(this.templates.header());
   }
 
-  this.datasets = _.map(o.datasets, function(oDataset) { return initializeDataset(that.$menu, oDataset, o.cssClasses); });
+  this.datasets = _.map(o.datasets, function(oDataset) {
+    return initializeDataset(that.$menu, oDataset, o.cssClasses);
+  });
   _.each(this.datasets, function(dataset) {
     var root = dataset.getRoot();
     if (root && root.parent().length === 0) {
@@ -57,7 +61,16 @@ function Dropdown(o) {
   });
 
   if (o.templates && o.templates.footer) {
-    this.$menu.append(_.templatify(o.templates.footer)());
+    this.templates.footer = _.templatify(o.templates.footer);
+    this.$menu.append(this.templates.footer());
+  }
+
+  if (o.templates && o.templates.empty) {
+    this.templates.empty = _.templatify(o.templates.empty);
+    this.$empty = DOM.element('<div class="' +
+      _.className(this.cssClasses.prefix, this.cssClasses.empty, true) + '">' +
+      '</div>');
+    this.$menu.append(this.$empty);
   }
 }
 
@@ -101,8 +114,18 @@ _.mixin(Dropdown.prototype, EventEmitter, {
     this.isEmpty = _.every(this.datasets, isDatasetEmpty);
 
     if (this.isEmpty) {
-      this._hide();
+      if (this.$empty) {
+        var html = this.templates.empty({
+          query: this.datasets[0] && this.datasets[0].query
+        });
+        this.$empty.html(html);
+      } else {
+        this._hide();
+      }
     } else if (this.isOpen) {
+      if (this.$empty) {
+        this.$empty.empty();
+      }
       this._show();
     }
 