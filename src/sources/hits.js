c3a7ef32f4dcb49d75148806c3097cd4e9dda3d4
Alexandre Stanislawski <alexandre.stanislawski@algolia.com>
2017-03-06 12:16:41 +0100

    fix(UA): add failsafe if params not provided
    Fixes #166

--- src/sources/hits.js
+++ src/sources/hits.js
@@ -7,6 +7,7 @@ var parseAlgoliaClientVersion = require('../common/parseAlgoliaClientVersion.js'
 module.exports = function search(index, params) {
   var algoliaVersion = parseAlgoliaClientVersion(index.as._ua);
   if (algoliaVersion && algoliaVersion[0] >= 3 && algoliaVersion[1] > 20) {
+    params = params || {};
     params.additionalUA = 'autocomplete.js ' + version;
   }
   return sourceFn;
