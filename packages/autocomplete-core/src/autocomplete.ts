c3b042d291cbf67fed7c991d208ff9d053acd8d2
François Chalifour <francois.chalifour@gmail.com>
2020-02-20 12:58:13 +0100

    chore(core): rename entry file

---
+++ packages/autocomplete-core/src/autocomplete.ts
@@ -0,0 +1,60 @@
+import { getDefaultProps } from './defaultProps';
+import { createStore } from './store';
+import { getPropGetters } from './propGetters';
+import { getAutocompleteSetters } from './setters';
+import { getCompletion } from './completion';
+
+import { PublicAutocompleteOptions, AutocompleteApi } from './types';
+
+function createAutocomplete<TItem extends {}>(
+  options: PublicAutocompleteOptions<TItem>
+): AutocompleteApi<TItem> {
+  const props = getDefaultProps(options);
+  const store = createStore(props.initialState, props.onStateChange);
+
+  const {
+    setHighlightedIndex,
+    setQuery,
+    setSuggestions,
+    setIsOpen,
+    setStatus,
+    setContext,
+  } = getAutocompleteSetters({ store, props });
+  const {
+    getEnvironmentProps,
+    getRootProps,
+    getFormProps,
+    getInputProps,
+    getItemProps,
+    getLabelProps,
+    getMenuProps,
+  } = getPropGetters({
+    store,
+    props,
+    setHighlightedIndex,
+    setQuery,
+    setSuggestions,
+    setIsOpen,
+    setStatus,
+    setContext,
+  });
+
+  return {
+    setHighlightedIndex,
+    setQuery,
+    setSuggestions,
+    setIsOpen,
+    setStatus,
+    setContext,
+    getEnvironmentProps,
+    getRootProps,
+    getFormProps,
+    getInputProps,
+    getItemProps,
+    getLabelProps,
+    getMenuProps,
+    getCompletion: () => getCompletion({ state: store.getState(), props }),
+  };
+}
+
+export { createAutocomplete };
