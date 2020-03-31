4842a67ac98addabb57053706d66a6dbe3a82459
François Chalifour <francois.chalifour@gmail.com>
2020-03-31 15:18:38 +0200

    chore: temporarilydisable release config for React package

--- ship.config.js
+++ ship.config.js
@@ -3,7 +3,8 @@
 const packages = [
   'packages/autocomplete-core',
   'packages/autocomplete-preset-algolia',
-  'packages/autocomplete-react',
+  // @TODO: toggle when we release the React package
+  // 'packages/autocomplete-react',
 ];
 
 module.exports = {
@@ -18,27 +19,29 @@ module.exports = {
   publishCommand({ tag }) {
     return `yarn publish --access public --tag ${tag}`;
   },
-  versionUpdated({ version, exec }) {
-    const update = ({ package, deps }) => {
-      /*
-        With caret(^), the dependencies need to be quoted, like the following:
-        > yarn workspace abc add "def@^1.0.0" "ghi@^1.0.0"
-      */
-      const depsWithVersion = deps.map(dep => `"${dep}@^${version}"`).join(' ');
-      exec(`yarn workspace ${package} add ${depsWithVersion}`);
-    };
+  versionUpdated({ exec }) {
+    // @TODO: toggle when we release the React package
+    // const update = ({ package, deps }) => {
+    //   /*
+    //     With caret(^), the dependencies need to be quoted, like the following:
+    //     > yarn workspace abc add "def@^1.0.0" "ghi@^1.0.0"
+    //   */
+    //   const depsWithVersion = deps.map(dep => `"${dep}@^${version}"`).join(' ');
+    //   exec(`yarn workspace ${package} add ${depsWithVersion}`);
+    // };
 
     // Ship.js read json and write like fs.writeFileSync(JSON.stringify(json, null, 2));
     // It causes a lint error with lerna.json file.
     exec('eslint lerna.json --fix');
 
-    update({
-      package: '@francoischalifour/autocomplete-react',
-      deps: [
-        '@francoischalifour/autocomplete-core',
-        '@francoischalifour/autocomplete-preset-algolia',
-      ],
-    });
+    // @TODO: toggle when we release the React package
+    // update({
+    //   package: '@francoischalifour/autocomplete-react',
+    //   deps: [
+    //     '@francoischalifour/autocomplete-core',
+    //     '@francoischalifour/autocomplete-preset-algolia',
+    //   ],
+    // });
   },
   // skip preparation if it contains only `chore` commits
   shouldPrepare: ({ releaseType, commitNumbersPerType }) => {
