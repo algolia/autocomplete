574dbc6ab153c29cb9229d9580359e86a6c902d5
François Chalifour <francois.chalifour@gmail.com>
2020-04-04 12:40:40 +0200

    fix(docsearch): hide search suggestions if none

--- packages/docsearch-react/src/NoResultsScreen.tsx
+++ packages/docsearch-react/src/NoResultsScreen.tsx
@@ -18,17 +18,18 @@ interface NoResultsScreenProps
 }
 
 export function NoResultsScreen(props: NoResultsScreenProps) {
+  const searchSuggestions: string[] = props.state.context.searchSuggestions;
+
   return (
     <div className="DocSearch-NoResults">
       <p className="DocSearch-Title">
         No results for "<strong>{props.state.query}</strong>".
       </p>
 
-      <p>
-        Try searching for{' '}
-        {(props.state.context.searchSuggestions as string[])
-          .slice(0, 3)
-          .reduce<React.ReactNode[]>(
+      {searchSuggestions.length > 0 && (
+        <p>
+          Try searching for{' '}
+          {searchSuggestions.slice(0, 3).reduce<React.ReactNode[]>(
             (acc, search) => [
               ...acc,
               acc.length > 0 ? ', ' : '',
@@ -46,8 +47,9 @@ export function NoResultsScreen(props: NoResultsScreenProps) {
             ],
             []
           )}
-        &nbsp;...
-      </p>
+          &nbsp;...
+        </p>
+      )}
 
       <p className="DocSearch-Help">
         If you believe this query should return results,
