42caf20187a7b716767588ba9081bfa181fa9e13
François Chalifour <francois.chalifour@gmail.com>
2020-04-24 16:20:44 +0200

    Revert "chore: use Babel to replace DocSearch verison in UA"
    This reverts commit 012be698540926df5a2ba11ee9c52cedcf00e3d7.

--- packages/docsearch-react/src/useSearchClient.ts
+++ packages/docsearch-react/src/useSearchClient.ts
@@ -1,10 +1,12 @@
 import React from 'react';
 import algoliasearch from 'algoliasearch/dist/algoliasearch-lite.esm.browser';
 
+import { version } from './version';
+
 export function useSearchClient(appId: string, apiKey: string) {
   const searchClient = React.useMemo(() => {
     const client = algoliasearch(appId, apiKey);
-    client.addAlgoliaAgent(`docsearch (${__VERSION__})`);
+    client.addAlgoliaAgent(`docsearch (${version})`);
 
     return client;
   }, [appId, apiKey]);
