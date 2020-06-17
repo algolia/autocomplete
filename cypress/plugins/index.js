7dac93e6a538f69bfe44bfdfe0fbb939b0e241e1
Shipow <shipowlata@gmail.com>
2020-06-17 14:07:03 +0200

    fix(ypress): Record reuslts

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,5 +1,7 @@
-import percyHealthCheck from '@percy/cypress/task';
+let percyHealthCheck = require("@percy/cypress/task");
 
-export default (on) => {
-  on('task', percyHealthCheck);
+module.exports = (on, config) => {
+  // `on` is used to hook into various events Cypress emits
+  // `config` is the resolved Cypress config
+  on("task", percyHealthCheck);
 };
