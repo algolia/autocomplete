949a24a9da50a6f1ca2c7b0a6891d0a09f63ae20
Shipow <shipowlata@gmail.com>
2020-06-17 12:25:15 +0200

    fix(ci): npm script + percy

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -15,7 +15,9 @@
 /**
  * @type {Cypress.PluginConfig}
  */
+
+let percyHealthCheck = require('@percy/cypress/task')
+
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
-}
+  on("task", percyHealthCheck);
+};
