d3a7275f03c8d397d797c9375a42bf977fc824ed
François Chalifour <francois.chalifour@gmail.com>
2020-07-01 15:00:46 +0200

    feat(docsearch): add `/` keyboard shortcut

--- packages/docsearch-react/src/useDocSearchKeyboardEvents.ts
+++ packages/docsearch-react/src/useDocSearchKeyboardEvents.ts
@@ -5,7 +5,11 @@ export function useDocSearchKeyboardEvents({ isOpen, onOpen, onClose }) {
     function onKeyDown(event: KeyboardEvent) {
       if (
         (event.keyCode === 27 && isOpen) ||
-        (event.key === 'k' && (event.metaKey || event.ctrlKey))
+        // The `Cmd+K` shortcut both opens and closes the modal.
+        (event.key === 'k' && (event.metaKey || event.ctrlKey)) ||
+        // The `/` shortcut opens but doesn't close the modal because it's
+        // a character.
+        (event.key === '/' && !isOpen)
       ) {
         event.preventDefault();
 
