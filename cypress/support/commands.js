58c39c59b8b0624fa4de90330654f63213f2f969
Shipow <shipowlata@gmail.com>
2020-07-04 16:18:02 +0200

    test(percy): add config

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -5,17 +5,24 @@ Cypress.Commands.add('darkmode', () => {
 });
 
 Cypress.Commands.add('viewportShot', (name) => {
-  const sizes = ['iphone-x', 'ipad-2', 'macbook-13', 'macbook-15'];
+  const sizes = ['iphone-x', 'ipad-2', 'macbook-11'];
+  cy.percySnapshot(`${name}`);
   sizes.forEach((size) => {
     if (Cypress._.isArray(size)) {
       cy.viewport(size[0], size[1]);
     } else {
       cy.viewport(size);
     }
-    cy.percySnapshot(`${name}__${size}`);
     cy.screenshot(`${name}__${size}`, { capture: 'viewport' });
-    cy.darkmode();
-    cy.percySnapshot(`${name}__${size}__darkmode`);
+  });
+  cy.darkmode();
+  cy.percySnapshot(`${name}__darkmode`);
+  sizes.forEach((size) => {
+    if (Cypress._.isArray(size)) {
+      cy.viewport(size[0], size[1]);
+    } else {
+      cy.viewport(size);
+    }
     cy.screenshot(`${name}__${size}__darkmode`, { capture: 'viewport' });
   });
 });
