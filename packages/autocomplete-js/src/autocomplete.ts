0cc8a44c3d27933bb581a2c258b2c238e8009904
François Chalifour <francois.chalifour@gmail.com>
2020-09-03 15:10:07 +0200

    feat(js): add magnifier glass as label

--- packages/autocomplete-js/src/autocomplete.ts
+++ packages/autocomplete-js/src/autocomplete.ts
@@ -110,8 +110,20 @@ export function autocomplete<TItem>({
 
   const labelProps = autocomplete.getLabelProps();
   setProperties(label, labelProps);
-  // @TODO have a magnifier glass
-  label.textContent = 'Search items';
+  label.innerHTML = `<svg
+  width="20"
+  height="20"
+  viewBox="0 0 20 20"
+>
+  <path
+    d="M14.386 14.386l4.0877 4.0877-4.0877-4.0877c-2.9418 2.9419-7.7115 2.9419-10.6533 0-2.9419-2.9418-2.9419-7.7115 0-10.6533 2.9418-2.9419 7.7115-2.9419 10.6533 0 2.9419 2.9418 2.9419 7.7115 0 10.6533z"
+    stroke="currentColor"
+    fill="none"
+    fillRule="evenodd"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  />
+</svg>`;
   label.classList.add('aa-Label');
 
   inputWrapper.classList.add('aa-InputWrapper');
@@ -207,7 +219,7 @@ export function autocomplete<TItem>({
     renderDropdown({ root: dropdown, sections, state });
   }
 
-  form.appendChild(label);
+  inputWrapper.appendChild(label);
   if (props.enableCompletion) {
     inputWrapper.appendChild(completion);
   }
