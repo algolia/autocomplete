daab82d940536687ea8e4d5bc8e73df5728de4a0
François Chalifour <francois.chalifour@gmail.com>
2020-09-03 15:10:24 +0200

    chore(js): add JavaScript example

---
+++ examples/js/app.js
@@ -0,0 +1,41 @@
+import algoliasearch from 'algoliasearch/lite';
+import {
+  autocomplete,
+  getAlgoliaHits,
+  reverseHighlightItem,
+} from '@algolia/autocomplete-js';
+
+const searchClient = algoliasearch(
+  'latency',
+  '6be0576ff61c053d5f9a3225e2a90f76'
+);
+
+autocomplete({
+  container: '#autocomplete',
+  getSources() {
+    return [
+      {
+        getInputValue: ({ suggestion }) => suggestion.query,
+        getSuggestions({ query }) {
+          return getAlgoliaHits({
+            searchClient,
+            queries: [
+              {
+                indexName: 'instant_search_demo_query_suggestions',
+                query,
+                params: {
+                  hitsPerPage: 4,
+                },
+              },
+            ],
+          });
+        },
+        templates: {
+          item({ item }) {
+            return reverseHighlightItem({ item, attribute: 'query' });
+          },
+        },
+      },
+    ];
+  },
+});
