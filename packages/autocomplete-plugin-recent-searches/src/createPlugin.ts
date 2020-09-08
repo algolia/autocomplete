3898ad06712effa3d211146f2fa4f1366d851caf
eunjae-lee <karis612@gmail.com>
2020-09-08 13:51:40 +0200

    add getSources back

--- packages/autocomplete-plugin-recent-searches/src/createPlugin.ts
+++ packages/autocomplete-plugin-recent-searches/src/createPlugin.ts
@@ -1,5 +1,3 @@
-import { getAlgoliaResults } from '@algolia/autocomplete-js';
-
 import { createRecentSearches } from './createRecentSearches';
 
 type Plugin = {
@@ -10,11 +8,7 @@ type Plugin = {
 
 type CreatePlugin = (options?: any) => Plugin;
 
-export const createPlugin: CreatePlugin = ({
-  searchClient,
-  indexName,
-  limit = 5,
-} = {}) => {
+export const createPlugin: CreatePlugin = ({ limit = 5 } = {}) => {
   const recentSearches = createRecentSearches({
     key: 'RECENT_SEARCHES',
     limit,
@@ -22,50 +16,33 @@ export const createPlugin: CreatePlugin = ({
 
   return {
     getSources: ({ query }) => {
-      getAlgoliaResults({
-        searchClient,
-        queries: [
-          {
-            indexName,
-            query,
-            params: {
-              hitsPerPage: limit,
-              // (2) Filter out QS already displayed in RS
-              facetFilters: recentSearches
-                .getAll()
-                .map((item) => [`objectID:-${item.query}`]),
-            },
+      return [
+        !query && {
+          getInputValue: ({ suggestion }) => suggestion.query,
+          getSuggestions() {
+            return recentSearches.getAll();
           },
-        ],
-      }).then(() => {
-        return [
-          !query && {
-            getInputValue: ({ suggestion }) => suggestion.query,
-            getSuggestions() {
-              return recentSearches.getAll();
-            },
-            onSelect({ suggestion }) {
-              recentSearches.add(suggestion);
-            },
-            templates: {
-              item({ item }) {
-                return `
-                  <div class="autocomplete-item">
-                    <div>
-                      <svg viewBox="0 0 22 22" width="16" height="16" fill="currentColor">
-                        <path d="M0 0h24v24H0z" fill="none"/>
-                        <path d="M13 3c-4.97 0-9 4.03-9 9H1l3.89 3.89.07.14L9 12H6c0-3.87 3.13-7 7-7s7 3.13 7 7-3.13 7-7 7c-1.93 0-3.68-.79-4.94-2.06l-1.42 1.42C8.27 19.99 10.51 21 13 21c4.97 0 9-4.03 9-9s-4.03-9-9-9zm-1 5v5l4.28 2.54.72-1.21-3.5-2.08V8H12z"/>
-                      </svg>
+          onSelect({ suggestion }) {
+            recentSearches.add(suggestion);
+          },
+          templates: {
+            item({ item }) {
+              return `
+                <div class="autocomplete-item">
+                  <div>
+                    <svg viewBox="0 0 22 22" width="16" height="16" fill="currentColor">
+                      <path d="M0 0h24v24H0z" fill="none"/>
+                      <path d="M13 3c-4.97 0-9 4.03-9 9H1l3.89 3.89.07.14L9 12H6c0-3.87 3.13-7 7-7s7 3.13 7 7-3.13 7-7 7c-1.93 0-3.68-.79-4.94-2.06l-1.42 1.42C8.27 19.99 10.51 21 13 21c4.97 0 9-4.03 9-9s-4.03-9-9-9zm-1 5v5l4.28 2.54.72-1.21-3.5-2.08V8H12z"/>
+                    </svg>
 
-                      <span>${item.query}</span>
-                    </div>
+                    <span>${item.query}</span>
                   </div>
-                `;
-              },
+                </div>
+              `;
             },
           },
-        ];
-      });
+        },
+      ];
     },
     onSubmit: ({ state }) => {
       recentSearches.add({
@@ -75,9 +52,6 @@ export const createPlugin: CreatePlugin = ({
     },
     onSelect: ({ suggestion }) => {
       recentSearches.add(suggestion);
-    },
-    getRecentSearches: () => {
-      return recentSearches.getAll();
     },
   };
 };
