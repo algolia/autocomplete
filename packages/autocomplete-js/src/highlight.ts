e276c5e91ee8028d8b7ac87a238375ec97caf730
François Chalifour <francois.chalifour@gmail.com>
2020-09-09 10:57:24 +0200

    feat(js): export snippeting utils

--- packages/autocomplete-js/src/highlight.ts
+++ packages/autocomplete-js/src/highlight.ts
@@ -1,7 +1,25 @@
 import {
   parseHighlightedAttribute,
   parseReverseHighlightedAttribute,
+  parseSnippetedAttribute,
+  parseReverseSnippetedAttribute,
 } from '@algolia/autocomplete-preset-algolia';
+
+type ParsedAttribute = { value: string; isHighlighted: boolean };
+
+function concatParts(
+  parts: ParsedAttribute[],
+  { highlightPreTag, highlightPostTag }
+) {
+  return parts.reduce<string>((acc, current) => {
+    return (
+      acc +
+      (current.isHighlighted
+        ? `${highlightPreTag}${current.value}${highlightPostTag}`
+        : current.value)
+    );
+  }, '');
+}
 
 type HighlightItemParams = {
   item: any;
@@ -16,19 +34,15 @@ export function highlightItem({
   highlightPreTag = '<mark>',
   highlightPostTag = '</mark>',
 }: HighlightItemParams) {
-  return parseHighlightedAttribute({
-    hit: item,
-    attribute,
-    highlightPreTag,
-    highlightPostTag,
-  }).reduce((acc, current) => {
-    return (
-      acc +
-      (current.isHighlighted
-        ? `${highlightPreTag}${current.value}${highlightPostTag}`
-        : current.value)
-    );
-  }, '');
+  return concatParts(
+    parseHighlightedAttribute({
+      hit: item,
+      attribute,
+      highlightPreTag,
+      highlightPostTag,
+    }),
+    { highlightPreTag, highlightPostTag }
+  );
 }
 
 export function reverseHighlightItem({
@@ -37,17 +51,47 @@ export function reverseHighlightItem({
   highlightPreTag = '<mark>',
   highlightPostTag = '</mark>',
 }: HighlightItemParams) {
-  return parseReverseHighlightedAttribute({
-    hit: item,
-    attribute,
-    highlightPreTag,
-    highlightPostTag,
-  }).reduce((acc, current) => {
-    return (
-      acc +
-      (current.isHighlighted
-        ? `${highlightPreTag}${current.value}${highlightPostTag}`
-        : current.value)
-    );
-  }, '');
+  return concatParts(
+    parseReverseHighlightedAttribute({
+      hit: item,
+      attribute,
+      highlightPreTag,
+      highlightPostTag,
+    }),
+    { highlightPreTag, highlightPostTag }
+  );
+}
+
+export function snippetItem({
+  item,
+  attribute,
+  highlightPreTag = '<mark>',
+  highlightPostTag = '</mark>',
+}: HighlightItemParams) {
+  return concatParts(
+    parseSnippetedAttribute({
+      hit: item,
+      attribute,
+      highlightPreTag,
+      highlightPostTag,
+    }),
+    { highlightPreTag, highlightPostTag }
+  );
+}
+
+export function reverseSnippetItem({
+  item,
+  attribute,
+  highlightPreTag = '<mark>',
+  highlightPostTag = '</mark>',
+}: HighlightItemParams) {
+  return concatParts(
+    parseReverseSnippetedAttribute({
+      hit: item,
+      attribute,
+      highlightPreTag,
+      highlightPostTag,
+    }),
+    { highlightPreTag, highlightPostTag }
+  );
 }
