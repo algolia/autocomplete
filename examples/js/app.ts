7bf1a4e6b43f0fa0f795cc82aa27477b12062048
François Chalifour <francois.chalifour@gmail.com>
2020-09-11 14:12:20 +0200

    docs(examples): update playground suggestions design

--- examples/js/app.ts
+++ examples/js/app.ts
@@ -24,7 +24,7 @@ autocomplete<QuerySuggestionHit>({
           indexName: 'instant_search_demo_query_suggestions',
           query,
           params: {
-            hitsPerPage: 4,
+            hitsPerPage: 10,
           },
         },
       ],
@@ -37,10 +37,15 @@ autocomplete<QuerySuggestionHit>({
           },
           templates: {
             item({ item }) {
-              return reverseHighlightItem<QuerySuggestionHit>({
-                item,
-                attribute: 'query',
-              });
+              return `
+                <div class="item-icon">${searchIcon}</div>
+                <div>
+                  ${reverseHighlightItem<QuerySuggestionHit>({
+                    item,
+                    attribute: 'query',
+                  })}
+                </div>
+              `;
             },
           },
         },
@@ -48,3 +53,14 @@ autocomplete<QuerySuggestionHit>({
     });
   },
 });
+
+const searchIcon = `<svg width="20" height="20" viewBox="0 0 20 20">
+  <path
+    d="M14.386 14.386l4.0877 4.0877-4.0877-4.0877c-2.9418 2.9419-7.7115 2.9419-10.6533 0-2.9419-2.9418-2.9419-7.7115 0-10.6533 2.9418-2.9419 7.7115-2.9419 10.6533 0 2.9419 2.9418 2.9419 7.7115 0 10.6533z"
+    stroke="currentColor"
+    fill="none"
+    fillRule="evenodd"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  />
+</svg>`;
