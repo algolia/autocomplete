ab67a96fc0aa8351a78ad702882bef095602ba59
François Chalifour <francois.chalifour@gmail.com>
2020-10-26 11:49:39 +0100

    feat: rename `item` to `hit` in highlight utils

--- examples/js/app.ts
+++ examples/js/app.ts
@@ -4,7 +4,7 @@ import {
   autocomplete,
   AutocompleteSource,
   getAlgoliaHits,
-  reverseHighlightItem,
+  reverseHighlightHit,
 } from '@algolia/autocomplete-js';
 import { createRecentSearchesPlugin } from '@algolia/autocomplete-plugin-recent-searches';
 import '@algolia/autocomplete-plugin-recent-searches/style';
@@ -51,8 +51,8 @@ autocomplete<Hit<QuerySuggestionHit>>({
               return `
                 <div class="item-icon">${searchIcon}</div>
                 <div>
-                  ${reverseHighlightItem({
-                    item,
+                  ${reverseHighlightHit({
+                    hit: item,
                     attribute: 'query',
                   })}
                 </div>
