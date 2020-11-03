370abaece212322d37a6954f30a3a52cbeb00ebb
François Chalifour <francois.chalifour@gmail.com>
2020-11-03 11:22:07 +0100

    feat: accept Promise in store `getAll`

--- packages/autocomplete-plugin-recent-searches/src/createStore.ts
+++ packages/autocomplete-plugin-recent-searches/src/createStore.ts
@@ -1,15 +1,18 @@
 import { RecentSearchesItem } from './types/RecentSearchesItem';
 
+// @TODO: reuse MaybePromise from autocomplete-core when we find a way to share the type
+export type MaybePromise<TResolution> = Promise<TResolution> | TResolution;
+
 export type RecentSearchesStore<TItem extends RecentSearchesItem> = {
   add(item: TItem): void;
   remove(id: string): void;
-  getAll(query?: string): TItem[];
+  getAll(query?: string): MaybePromise<TItem[]>;
 };
 
 type CreateRecentSearchesStoreParams<TItem extends RecentSearchesItem> = {
   onAdd(item: TItem): void;
   onRemove(id: string): void;
-  getAll(query?: string): TItem[];
+  getAll(query?: string): MaybePromise<TItem[]>;
 };
 
 export function createStore<TItem extends RecentSearchesItem>(
