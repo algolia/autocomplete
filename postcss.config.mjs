25cc261cfa96aa27947c120545c70411be1ffc05
François Chalifour <francois.chalifour@gmail.com>
2020-11-09 11:10:51 +0100

    feat: add `autoprefixer`

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,7 @@
+import autoprefixer from 'autoprefixer';
 import cssnano from 'cssnano';
 import sass from 'postcss-node-sass';
 
 export default {
-  plugins: [sass, cssnano],
+  plugins: [autoprefixer, sass, cssnano],
 };
