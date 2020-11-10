fc6e6c8b68cdca4b025c706642294eb122ca6eab
François Chalifour <francois.chalifour@gmail.com>
2020-11-10 16:46:13 +0100

    wip

--- scripts/codesandbox/install.sh
+++ scripts/codesandbox/install.sh
@@ -1,4 +1,4 @@
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.0/install.sh | bash
 
-export NVM_DIR="/home/node/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+echo 'export NVM_DIR=/home/node/.nvm' >> $BASH_ENV
+echo 'source $NVM_DIR/nvm.sh' >> $BASH_ENV
