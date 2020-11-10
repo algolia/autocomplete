2b4be21dabe90942d142fc59c0c4a1e2e85f9c8c
François Chalifour <francois.chalifour@gmail.com>
2020-11-10 16:40:51 +0100

    wip

--- scripts/codesandbox/install.sh
+++ scripts/codesandbox/install.sh
@@ -1,4 +1,4 @@
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.0/install.sh | bash
 
-echo 'export NVM_DIR=$HOME/.nvm' >> $BASH_ENV
-echo 'source $NVM_DIR/nvm.sh' >> $BASH_ENV
+ls .
+echo 'source ~/.nvm/nvm.sh' >> $BASH_ENV
