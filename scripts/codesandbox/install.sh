84b47583162e0d16889411b598dad9aa86d8e701
François Chalifour <francois.chalifour@gmail.com>
2020-11-10 11:32:19 +0100

    wip

--- scripts/codesandbox/install.sh
+++ scripts/codesandbox/install.sh
@@ -1,5 +1,6 @@
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.0/install.sh | bash
 
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # Load nvm
+
+nvm use
