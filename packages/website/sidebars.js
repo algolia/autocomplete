b747b819372d2b906154dcaf256ec01bd70d75f4
François Chalifour <francois.chalifour@gmail.com>
2020-11-16 14:52:20 +0100

    docs: add API in navbar

--- packages/website/sidebars.js
+++ packages/website/sidebars.js
@@ -16,37 +16,25 @@ module.exports = {
     ],
     Workflow: ['debugging', 'upgrading'],
     Guides: ['creating-a-renderer'],
-    API: [
-      {
-        type: 'category',
-        label: 'autocomplete-core',
-        items: ['createAutocomplete'],
-      },
-      {
-        type: 'category',
-        label: 'autocomplete-js',
-        items: [
-          'autocomplete-js',
-          'getAlgoliaHits-js',
-          'getAlgoliaResults-js',
-          'highlightHit',
-          'reverseHighlightHit',
-          'snippetHit',
-          'reverseSnippetHit',
-        ],
-      },
-      {
-        type: 'category',
-        label: 'autocomplete-preset-algolia',
-        items: [
-          'getAlgoliaHits',
-          'getAlgoliaResults',
-          'parseAlgoliaHitHighlight',
-          'parseAlgoliaHitReverseHighlight',
-          'parseAlgoliaHitSnippet',
-          'parseAlgoliaHitReverseSnippet',
-        ],
-      },
+  },
+  api: {
+    'autocomplete-core': ['createAutocomplete'],
+    'autocomplete-js': [
+      'autocomplete-js',
+      'getAlgoliaHits-js',
+      'getAlgoliaResults-js',
+      'highlightHit',
+      'reverseHighlightHit',
+      'snippetHit',
+      'reverseSnippetHit',
+    ],
+    'autocomplete-preset-algolia': [
+      'getAlgoliaHits',
+      'getAlgoliaResults',
+      'parseAlgoliaHitHighlight',
+      'parseAlgoliaHitReverseHighlight',
+      'parseAlgoliaHitSnippet',
+      'parseAlgoliaHitReverseSnippet',
     ],
   },
 };
