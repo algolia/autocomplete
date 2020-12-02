f22a6c8677c3ec26b8a4333d5b8ab411a2bd9313
François Chalifour <francois.chalifour@gmail.com>
2020-12-02 11:51:09 +0100

    test(js): update snapshot

--- packages/autocomplete-js/src/__tests__/autocomplete.test.ts
+++ packages/autocomplete-js/src/__tests__/autocomplete.test.ts
@@ -64,21 +64,26 @@ describe('autocomplete-js', () => {
                 for="autocomplete-input"
                 id="autocomplete-label"
               >
-                <svg
-                  height="20"
-                  viewBox="0 0 20 20"
-                  width="20"
+                <button
+                  class="aa-SubmitButton"
+                  type="submit"
                 >
-                  <path
-                    d="M14.386 14.386l4.0877 4.0877-4.0877-4.0877c-2.9418 2.9419-7.7115 2.9419-10.6533 0-2.9419-2.9418-2.9419-7.7115 0-10.6533 2.9418-2.9419 7.7115-2.9419 10.6533 0 2.9419 2.9418 2.9419 7.7115 0 10.6533z"
-                    fill="none"
-                    fill-rule="evenodd"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="1.4"
-                  />
-                </svg>
+                  <svg
+                    height="20"
+                    viewBox="0 0 20 20"
+                    width="20"
+                  >
+                    <path
+                      d="M14.386 14.386l4.0877 4.0877-4.0877-4.0877c-2.9418 2.9419-7.7115 2.9419-10.6533 0-2.9419-2.9418-2.9419-7.7115 0-10.6533 2.9418-2.9419 7.7115-2.9419 10.6533 0 2.9419 2.9418 2.9419 7.7115 0 10.6533z"
+                      fill="none"
+                      fill-rule="evenodd"
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="1.4"
+                    />
+                  </svg>
+                </button>
               </label>
               <button
                 class="aa-ResetButton"
