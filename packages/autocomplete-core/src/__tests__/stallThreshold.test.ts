0611121b23d3422227283020833c8649eb035430
François Chalifour <francois.chalifour@gmail.com>
2020-12-03 15:43:36 +0100

    test(core): test `stallThreshold` prop

---
+++ packages/autocomplete-core/src/__tests__/stallThreshold.test.ts
@@ -0,0 +1,107 @@
+import userEvent from '@testing-library/user-event';
+
+import { createAutocomplete } from '../createAutocomplete';
+
+function defer<TValue>(fn: () => TValue, timeout: number) {
+  return new Promise<TValue>((resolve) => {
+    setTimeout(() => resolve(fn()), timeout);
+  });
+}
+
+describe('stallThreshold', () => {
+  test('sets the experience to stalled after 300ms', async () => {
+    const onStateChange = jest.fn((x) => x);
+    const autocomplete = createAutocomplete({
+      onStateChange,
+      getSources() {
+        return defer(() => {
+          return [
+            {
+              getItems() {
+                return [{ label: '1' }, { label: 2 }];
+              },
+            },
+          ];
+        }, 500);
+      },
+    });
+
+    const inputElement = document.createElement('input');
+    const inputProps = autocomplete.getInputProps({ inputElement });
+    inputElement.addEventListener('input', inputProps.onChange);
+    document.body.appendChild(inputElement);
+
+    userEvent.type(inputElement, 'a');
+
+    expect(onStateChange).toHaveBeenLastCalledWith({
+      prevState: expect.anything(),
+      state: expect.objectContaining({ status: 'loading' }),
+    });
+
+    await defer(() => {}, 300);
+
+    expect(onStateChange).toHaveBeenLastCalledWith({
+      prevState: expect.anything(),
+      state: expect.objectContaining({ status: 'stalled' }),
+    });
+
+    await defer(() => {}, 200);
+
+    expect(onStateChange).toHaveBeenLastCalledWith({
+      prevState: expect.anything(),
+      state: expect.objectContaining({ status: 'idle' }),
+    });
+  });
+
+  test('allows custom stall threshold', async () => {
+    const onStateChange = jest.fn((x) => x);
+    const autocomplete = createAutocomplete({
+      stallThreshold: 400,
+      onStateChange,
+      getSources() {
+        return defer(() => {
+          return [
+            {
+              getItems() {
+                return [{ label: '1' }, { label: 2 }];
+              },
+            },
+          ];
+        }, 500);
+      },
+    });
+
+    const inputElement = document.createElement('input');
+    const inputProps = autocomplete.getInputProps({ inputElement });
+    inputElement.addEventListener('input', inputProps.onChange);
+    document.body.appendChild(inputElement);
+
+    userEvent.type(inputElement, 'a');
+
+    expect(onStateChange).toHaveBeenLastCalledWith({
+      prevState: expect.anything(),
+      state: expect.objectContaining({ status: 'loading' }),
+    });
+
+    await defer(() => {}, 300);
+
+    expect(onStateChange).toHaveBeenLastCalledWith({
+      prevState: expect.anything(),
+      state: expect.objectContaining({ status: 'loading' }),
+    });
+
+    await defer(() => {}, 100);
+
+    expect(onStateChange).toHaveBeenLastCalledWith({
+      prevState: expect.anything(),
+      state: expect.objectContaining({ status: 'stalled' }),
+    });
+
+    await defer(() => {}, 100);
+
+    expect(onStateChange).toHaveBeenLastCalledWith({
+      prevState: expect.anything(),
+      state: expect.objectContaining({ status: 'idle' }),
+    });
+  });
+});
