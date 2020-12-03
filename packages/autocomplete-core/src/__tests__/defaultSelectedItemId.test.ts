36b2311cb93c9a4ebc33151abc34298fc6511eb9
François Chalifour <francois.chalifour@gmail.com>
2020-12-03 16:04:48 +0100

    test(core): prepare tests for `defaultSelectedItemId`

---
+++ packages/autocomplete-core/src/__tests__/defaultSelectedItemId.test.ts
@@ -0,0 +1,7 @@
+describe('defaultSelectedItemId', () => {
+  test.todo('selects defaultSelectedItemId on input');
+  test.todo('selects defaultSelectedItemId with openOnFocus on reset');
+  test.todo('selects defaultSelectedItemId on focus');
+  test.todo('selects defaultSelectedItemId if not null when ArrowDown');
+  test.todo('selects defaultSelectedItemId if not null when ArrowUp');
+});
