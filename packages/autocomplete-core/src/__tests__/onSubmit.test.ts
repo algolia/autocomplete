66e3a4909131b7a0eb7081da2a1c5296e0fd0faa
François Chalifour <francois.chalifour@gmail.com>
2020-12-03 14:34:47 +0100

    test(core): test `onSubmit` prop

---
+++ packages/autocomplete-core/src/__tests__/onSubmit.test.ts
@@ -0,0 +1,41 @@
+import userEvent from '@testing-library/user-event';
+
+import { createAutocomplete } from '../createAutocomplete';
+
+describe('onSubmit', () => {
+  test('calls the user provided onSubmit function', () => {
+    const onSubmitProp = jest.fn();
+    const { getFormProps } = createAutocomplete({ onSubmit: onSubmitProp });
+
+    const form = document.createElement('form');
+    const { onSubmit } = getFormProps({ inputElement: null });
+    form.addEventListener('submit', onSubmit);
+    const submitButton = document.createElement('button');
+    submitButton.type = 'submit';
+
+    document.body.appendChild(form);
+    form.appendChild(submitButton);
+
+    userEvent.click(submitButton);
+
+    expect(onSubmitProp).toHaveBeenCalledTimes(1);
+    expect(onSubmitProp).toHaveBeenCalledWith({
+      event: expect.any(Event),
+      setCollections: expect.any(Function),
+      setContext: expect.any(Function),
+      setIsOpen: expect.any(Function),
+      setQuery: expect.any(Function),
+      setSelectedItemId: expect.any(Function),
+      setStatus: expect.any(Function),
+      state: {
+        collections: [],
+        completion: null,
+        context: {},
+        isOpen: false,
+        query: '',
+        selectedItemId: null,
+        status: 'idle',
+      },
+    });
+  });
+});
