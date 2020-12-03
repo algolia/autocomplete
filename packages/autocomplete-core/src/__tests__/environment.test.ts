b593a31e28b03646ef5bbfc4836a734956c2e8a3
François Chalifour <francois.chalifour@gmail.com>
2020-12-03 23:07:16 +0100

    test(core): test `environment`

--- packages/autocomplete-core/src/__tests__/environment.test.ts
+++ packages/autocomplete-core/src/__tests__/environment.test.ts
@@ -1,4 +1,34 @@
+import { createAutocomplete } from '../createAutocomplete';
+
 describe('environment', () => {
-  test.todo('defaults to the global objbect');
-  test.todo('allows providing a custom environment');
+  test('defaults to the global object', () => {
+    jest.spyOn(global, 'setTimeout');
+    const autocomplete = createAutocomplete({ openOnFocus: true });
+    const inputElement = document.createElement('input');
+    const inputProps = autocomplete.getInputProps({ inputElement });
+    inputElement.addEventListener('focus', inputProps.onFocus);
+    document.body.appendChild(inputElement);
+
+    // `focus()` triggers `onInput` which calls `environment.setTimeout`
+    inputElement.focus();
+
+    expect(global.setTimeout).toHaveBeenCalledTimes(1);
+  });
+
+  test('allows providing a custom environment', () => {
+    const environment = {
+      ...global,
+      setTimeout: jest.fn(),
+    };
+    const autocomplete = createAutocomplete({ environment, openOnFocus: true });
+    const inputElement = document.createElement('input');
+    const inputProps = autocomplete.getInputProps({ inputElement });
+    inputElement.addEventListener('focus', inputProps.onFocus);
+    document.body.appendChild(inputElement);
+
+    // `focus()` triggers `onInput` which calls `environment.setTimeout`
+    inputElement.focus();
+
+    expect(environment.setTimeout).toHaveBeenCalledTimes(1);
+  });
 });
