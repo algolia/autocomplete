bf7d47315c8e657672dce39bb4763e64920ee7e2
François Chalifour <francois.chalifour@gmail.com>
2020-12-03 16:05:56 +0100

    test(core): prepare tests for `openOnFocus`

---
+++ packages/autocomplete-core/src/__tests__/openOnFocus.test.ts
@@ -0,0 +1,8 @@
+describe('openOnFocus', () => {
+  test.todo('triggers a search on reset');
+  test.todo('opens panel on reset');
+  test.todo('sets selectedItemId on reset');
+  test.todo('triggers a search on focus without query');
+  test.todo('sets the collections without query');
+  test.todo('opens panel without query');
+});
