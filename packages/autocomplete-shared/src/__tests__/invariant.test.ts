6e2c1b06f324b38e17a703ab6e4b858594ccf7d5
François Chalifour <francois.chalifour@gmail.com>
2020-12-09 15:49:14 +0100

    test(shared): test `warn` and `invariant` only in dev

--- packages/autocomplete-shared/src/__tests__/invariant.test.ts
+++ packages/autocomplete-shared/src/__tests__/invariant.test.ts
@@ -1,15 +1,17 @@
 import { invariant } from '../invariant';
 
 describe('invariant', () => {
-  test('throws when the condition is unmet', () => {
-    expect(() => {
-      invariant(false, 'invariant');
-    }).toThrow('[Autocomplete] invariant');
-  });
+  if (__DEV__) {
+    test('throws when the condition is unmet', () => {
+      expect(() => {
+        invariant(false, 'invariant');
+      }).toThrow('[Autocomplete] invariant');
+    });
 
-  test('does not throw when the condition is met', () => {
-    expect(() => {
-      invariant(true, 'invariant');
-    }).not.toThrow();
-  });
+    test('does not throw when the condition is met', () => {
+      expect(() => {
+        invariant(true, 'invariant');
+      }).not.toThrow();
+    });
+  }
 });
--- packages/autocomplete-shared/src/__tests__/warn.test.ts
+++ packages/autocomplete-shared/src/__tests__/warn.test.ts
@@ -3,36 +3,44 @@
 import { warn } from '../warn';
 
 describe('warn', () => {
-  test('logs when the condition is unmet', () => {
-    expect(() => {
-      warn(false, 'warning');
-    }).toWarnDev('[Autocomplete] warning');
-  });
+  if (__DEV__) {
+    test('logs when the condition is unmet', () => {
+      expect(() => {
+        warn(false, 'warning');
+      }).toWarnDev('[Autocomplete] warning');
+    });
 
-  test('does not log when the condition is met', () => {
-    expect(() => {
-      warn(true, 'warning');
-    }).not.toWarnDev();
-  });
+    test('does not log when the condition is met', () => {
+      expect(() => {
+        warn(true, 'warning');
+      }).not.toWarnDev();
+    });
 
-  test('trims the message', () => {
-    expect(() => {
-      warn(false, '\nwarning! ');
-    }).toWarnDev('[Autocomplete] warning!');
-  });
+    test('trims the message', () => {
+      expect(() => {
+        warn(false, '\nwarning! ');
+      }).toWarnDev('[Autocomplete] warning!');
+    });
 
-  test('warns a message a single time', () => {
-    const originalConsoleWarn = console.warn;
-    console.warn = jest.fn();
+    test('warns a message a single time', () => {
+      const originalConsoleWarn = console.warn;
+      console.warn = jest.fn();
 
-    warn(false, 'warning1');
-    warn(false, 'warning1');
-    warn(false, 'warning2');
+      warn(false, 'warning1');
+      warn(false, 'warning1');
+      warn(false, 'warning2');
 
-    expect(console.warn).toHaveBeenCalledTimes(2);
-    expect(console.warn).toHaveBeenNthCalledWith(1, '[Autocomplete] warning1');
-    expect(console.warn).toHaveBeenNthCalledWith(2, '[Autocomplete] warning2');
+      expect(console.warn).toHaveBeenCalledTimes(2);
+      expect(console.warn).toHaveBeenNthCalledWith(
+        1,
+        '[Autocomplete] warning1'
+      );
+      expect(console.warn).toHaveBeenNthCalledWith(
+        2,
+        '[Autocomplete] warning2'
+      );
 
-    console.warn = originalConsoleWarn;
-  });
+      console.warn = originalConsoleWarn;
+    });
+  }
 });
