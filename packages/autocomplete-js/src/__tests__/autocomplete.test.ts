4a3d11c87eef8fa132ca6e63dc1e86dc04a46865
François Chalifour <francois.chalifour@gmail.com>
2021-01-21 12:28:26 +0100

    test(js): update snapshots

--- packages/autocomplete-js/src/__tests__/autocomplete.test.ts
+++ packages/autocomplete-js/src/__tests__/autocomplete.test.ts
@@ -92,8 +92,8 @@ describe('autocomplete-js', () => {
                     stroke-dasharray="164.93361431346415 56.97787143782138"
                     stroke-width="6"
                   >
-
-
+                    
+        
                     <animatetransform
                       attributeName="transform"
                       dur="1s"
@@ -102,7 +102,7 @@ describe('autocomplete-js', () => {
                       type="rotate"
                       values="0 50 50;90 50 50;180 50 50;360 50 50"
                     />
-
+                    
 
                   </circle>
                 </svg>
