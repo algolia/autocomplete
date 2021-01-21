dc2e5fa432383b4d4d399195b199c5ab33091bc8
Yannick Croissant <yannick.croissant@gmail.com>
2021-01-21 18:01:36 +0100

    test(core): add tests for getEnvironmentProps onTouchStart

--- packages/autocomplete-core/src/__tests__/getEnvironmentProps.test.ts
+++ packages/autocomplete-core/src/__tests__/getEnvironmentProps.test.ts
@@ -5,9 +5,99 @@ describe('getEnvironmentProps', () => {
   test.todo('forwards the remaining props');
 
   describe('onTouchStart', () => {
-    test.todo('is a noop when panel is not open');
-    test.todo('is a noop when the event target is the input element');
-    test.todo('blurs input if the target is outside Autocomplete');
+    test('is a noop when panel is not open', () => {
+      const {
+        getEnvironmentProps,
+        inputElement,
+        formElement,
+      } = createPlayground(createAutocomplete, {});
+      const panelElement = document.createElement('div');
+
+      jest.spyOn(inputElement, 'blur');
+
+      const { onTouchStart } = getEnvironmentProps({
+        inputElement,
+        formElement,
+        panelElement,
+      });
+      window.addEventListener('touchstart', onTouchStart);
+
+      // Focus input (with the panel closed)
+      inputElement.focus();
+
+      // Dispatch TouchStart event on window
+      const customEvent = new CustomEvent('touchstart', {
+        bubbles: true,
+      });
+      window.dispatchEvent(customEvent);
+
+      expect(inputElement.blur).toHaveBeenCalledTimes(0);
+    });
+
+    test('is a noop when the event target is the input element', () => {
+      const {
+        getEnvironmentProps,
+        inputElement,
+        formElement,
+      } = createPlayground(createAutocomplete, {
+        openOnFocus: true,
+      });
+      const panelElement = document.createElement('div');
+
+      jest.spyOn(inputElement, 'blur');
+
+      const { onTouchStart } = getEnvironmentProps({
+        inputElement,
+        formElement,
+        panelElement,
+      });
+      window.addEventListener('touchstart', onTouchStart);
+
+      // Focus input (opens the panel)
+      inputElement.focus();
+
+      // Dispatch TouchStart event on the input (bubbles to window)
+      const customEvent = new CustomEvent('touchstart', {
+        bubbles: true,
+      });
+      inputElement.dispatchEvent(customEvent);
+
+      expect(inputElement.blur).toHaveBeenCalledTimes(0);
+    });
+
+    test('blurs input if the target is outside Autocomplete', () => {
+      const onStateChange = jest.fn();
+      const {
+        getEnvironmentProps,
+        inputElement,
+        formElement,
+      } = createPlayground(createAutocomplete, {
+        onStateChange,
+        initialState: { isOpen: true },
+      });
+      const panelElement = document.createElement('div');
+
+      const { onTouchStart } = getEnvironmentProps({
+        inputElement,
+        formElement,
+        panelElement,
+      });
+      window.addEventListener('touchstart', onTouchStart);
+
+      // Dispatch TouchStart event on window (so, outside of Autocomplete)
+      const customEvent = new CustomEvent('touchstart', {
+        bubbles: true,
+      });
+      window.document.dispatchEvent(customEvent);
+
+      expect(onStateChange).toHaveBeenLastCalledWith({
+        prevState: expect.anything(),
+        state: expect.objectContaining({
+          activeItemId: null,
+          isOpen: false,
+        }),
+      });
+    });
   });
 
   describe('onTouchMove', () => {
