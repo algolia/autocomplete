931b3679c900b0e2e989019043e35c45fe2f7f22
Shipow <shipowlata@gmail.com>
2021-01-24 11:49:12 +0100

    fix(css): theme classic scss

--- examples/js/app.tsx
+++ examples/js/app.tsx
@@ -62,12 +62,20 @@ autocomplete({
           });
         },
         templates: {
+          header() {
+            return (
+              <Fragment>
+                <span>Products</span>
+                <div class="aa-SourceHeaderLine"></div>
+              </Fragment>
+            );
+          },
           item({ item }) {
             return <ProductItem hit={item} />;
           },
           empty() {
             return (
-              <div className="aa-ItemContent">No results for this query.</div>
+              <div className="aa-ItemContent"><div className="aa-ItemContentTitle">No results for this query.</div></div>
             );
           },
         },
@@ -83,14 +91,48 @@ type ProductItemProps = {
 function ProductItem({ hit }: ProductItemProps) {
   return (
     <Fragment>
-    <div className="aa-ItemSourceIcon">
-      <img src={hit.image} alt={hit.name} width="20" height="20" />
-    </div>
-    <div className="aa-ItemContent">
-      <div className="aa-ItemContentTitle">
-        {highlightHit<ProductHit>({ hit, attribute: 'name' })}
+      <div className="aa-ItemSourceIcon">
+        <img src={hit.image} alt={hit.name} width="20" height="20" />
       </div>
-    </div>
-  </Fragment>
+      <div className="aa-ItemContent">
+        <div className="aa-ItemContentTitle">
+          {highlightHit<ProductHit>({ hit, attribute: 'name' })}
+        </div>
+      </div>
+      <button className="aa-ItemActionButton" type="button" title="select">
+        <svg fill="currentColor" viewBox="0 0 24 24" width="20" height="20">
+          <path d="M18.984 6.984h2.016v6h-15.188l3.609 3.609-1.406 1.406-6-6 6-6 1.406 1.406-3.609 3.609h13.172v-4.031z"></path>
+        </svg>
+      </button>
+    </Fragment>
   );
+}
+
+const darkSwitch = document.getElementById("darkSwitch");
+
+if (darkSwitch) {
+  initTheme();
+  darkSwitch.addEventListener("change", () => {
+    resetTheme();
+  });
+}
+
+function initTheme() {
+  const darkThemeSelected =
+    localStorage.getItem("darkSwitch") !== null &&
+    localStorage.getItem("darkSwitch") === "dark";
+  darkSwitch.checked = darkThemeSelected;
+  darkThemeSelected
+    ? document.body.setAttribute("data-theme", "dark")
+    : document.body.removeAttribute("data-theme");
+}
+
+function resetTheme() {
+  if (darkSwitch.checked) {
+    document.body.setAttribute("data-theme", "dark");
+    localStorage.setItem("darkSwitch", "dark");
+  } else {
+    document.body.removeAttribute("data-theme");
+    localStorage.removeItem("darkSwitch");
+  }
 }
