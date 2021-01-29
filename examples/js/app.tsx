7765c6a3e6752351354b0b4a072d4087899eb5d8
François Chalifour <francois.chalifour@gmail.com>
2021-01-29 14:37:33 +0100

    feat(theme): update description style

--- examples/js/app.tsx
+++ examples/js/app.tsx
@@ -17,7 +17,11 @@ import '@algolia/autocomplete-theme-classic';
 
 import { shortcutsPlugin } from './shortcutsPlugin';
 
-type Product = { name: string; image: string };
+type Product = {
+  name: string;
+  image: string;
+  description: string;
+};
 type ProductHit = Hit<Product>;
 
 const appId = 'latency';
@@ -67,7 +71,8 @@ autocomplete({
                 indexName: 'instant_search',
                 query,
                 params: {
-                  attributesToSnippet: ['description:25'],
+                  attributesToSnippet: ['name:10', 'description:35'],
+                  snippetEllipsisText: '…',
                 },
               },
             ],
@@ -112,13 +117,17 @@ function ProductItem({ hit }: ProductItemProps) {
       </div>
       <div className="aa-ItemContent">
         <div className="aa-ItemContentTitle">
-          {highlightHit<ProductHit>({ hit, attribute: 'name' })}
+          {snippetHit<ProductHit>({ hit, attribute: 'name' })}
         </div>
         <div className="aa-ItemContentDescription">
           {snippetHit<ProductHit>({ hit, attribute: 'description' })}
         </div>
       </div>
-      <button className="aa-ItemActionButton" type="button" title="select">
+      <button
+        className="aa-ItemActionButton aa-TouchOnly aa-ActiveOnly"
+        type="button"
+        title="Select"
+      >
         <svg fill="currentColor" viewBox="0 0 24 24" width="20" height="20">
           <path d="M18.984 6.984h2.016v6h-15.188l3.609 3.609-1.406 1.406-6-6 6-6 1.406 1.406-3.609 3.609h13.172v-4.031z"></path>
         </svg>
