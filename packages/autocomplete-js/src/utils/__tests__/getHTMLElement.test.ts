161b5fe5b34dfab3ed29252984aff2d1b5d96e3c
François Chalifour <francois.chalifour@gmail.com>
2021-02-01 19:55:44 +0100

    test: add todos

---
+++ packages/autocomplete-js/src/utils/__tests__/getHTMLElement.test.ts
@@ -0,0 +1,7 @@
+describe('getHTMLElement', () => {
+  test.todo('with element returns the element');
+
+  test.todo('with a string returns the element if exists');
+
+  test.todo('with a string throws invariant if does not exist');
+});
