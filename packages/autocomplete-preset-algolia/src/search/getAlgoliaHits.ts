fab2d57c3d0c08ab2e46fad80224fe9f740988eb
François Chalifour <francoischalifour@users.noreply.github.com>
2021-02-01 14:46:18 +0100

    feat(js): add JS user agent to Algolia requests (#420)

--- packages/autocomplete-preset-algolia/src/search/getAlgoliaHits.ts
+++ packages/autocomplete-preset-algolia/src/search/getAlgoliaHits.ts
@@ -5,18 +5,21 @@ import { search, SearchParams } from './search';
 export function getAlgoliaHits<TRecord>({
   searchClient,
   queries,
+  userAgents,
 }: SearchParams): Promise<Array<Array<Hit<TRecord>>>> {
-  return search<TRecord>({ searchClient, queries }).then((response) => {
-    const results = response.results;
+  return search<TRecord>({ searchClient, queries, userAgents }).then(
+    (response) => {
+      const results = response.results;
 
-    return results.map((result) =>
-      result.hits.map((hit) => {
-        return {
-          ...hit,
-          __autocomplete_indexName: result.index,
-          __autocomplete_queryID: result.queryID,
-        };
-      })
-    );
-  });
+      return results.map((result) =>
+        result.hits.map((hit) => {
+          return {
+            ...hit,
+            __autocomplete_indexName: result.index,
+            __autocomplete_queryID: result.queryID,
+          };
+        })
+      );
+    }
+  );
 }
