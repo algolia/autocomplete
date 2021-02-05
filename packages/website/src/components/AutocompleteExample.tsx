46fb05b124c81d77f23a2f42066efdcefebfeb00
François Chalifour <francois.chalifour@gmail.com>
2021-02-05 12:58:06 +0100

    feat(website): update `AutocompleteExample` component

---
+++ packages/website/src/components/AutocompleteExample.tsx
@@ -0,0 +1,34 @@
+import { autocomplete, AutocompleteOptions } from '@algolia/autocomplete-js';
+import React, { createElement, Fragment, useEffect, useRef } from 'react';
+import { render } from 'react-dom';
+
+import '@algolia/autocomplete-theme-classic';
+
+export function AutocompleteExample<TItem extends {}>(
+  props: Omit<AutocompleteOptions<TItem>, 'container'>
+) {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!containerRef.current) {
+      return undefined;
+    }
+
+    const search = autocomplete({
+      container: containerRef.current,
+      debug: process.env.NODE_ENV === 'development',
+      placeholder: 'Search',
+      renderer: { createElement, Fragment },
+      render({ children }, root) {
+        render(children as any, root);
+      },
+      ...props,
+    });
+
+    return () => {
+      search.destroy();
+    };
+  }, [props]);
+
+  return <div ref={containerRef} style={{ padding: '1rem 0' }} />;
+}
