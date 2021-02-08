9e20533515906183eb3225f7dfe63bf78084825b
François Chalifour <francois.chalifour@gmail.com>
2021-02-08 13:36:41 +0100

    feat(js): wrap item action buttons in container

--- packages/autocomplete-plugin-query-suggestions/src/getTemplates.tsx
+++ packages/autocomplete-plugin-query-suggestions/src/getTemplates.tsx
@@ -36,19 +36,26 @@ export function getTemplates<TItem extends QuerySuggestionsHit>({
               })}
             </div>
           </div>
-          <button
-            className="aa-ItemActionButton"
-            title={`Fill query with "${item.query}"`}
-            onClick={(event) => {
-              event.stopPropagation();
-              onTapAhead(item);
-            }}
-          >
-            <svg viewBox="0 0 24 24" fill="currentColor" width="18" height="18">
-              <rect fill="none" height="24" width="24" />
-              <path d="M5,15h2V8.41L18.59,20L20,18.59L8.41,7H15V5H5V15z" />
-            </svg>
-          </button>
+          <div className="aa-ItemActions">
+            <button
+              className="aa-ItemActionButton"
+              title={`Fill query with "${item.query}"`}
+              onClick={(event) => {
+                event.stopPropagation();
+                onTapAhead(item);
+              }}
+            >
+              <svg
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                width="18"
+                height="18"
+              >
+                <rect fill="none" height="24" width="24" />
+                <path d="M5,15h2V8.41L18.59,20L20,18.59L8.41,7H15V5H5V15z" />
+              </svg>
+            </button>
+          </div>
         </Fragment>
       );
     },
--- packages/autocomplete-plugin-recent-searches/src/getTemplates.tsx
+++ packages/autocomplete-plugin-recent-searches/src/getTemplates.tsx
@@ -29,29 +29,31 @@ export function getTemplates<TItem extends RecentSearchesItem>({
               })}
             </div>
           </div>
-          <button
-            className="aa-ItemActionButton"
-            title="Remove this search"
-            onClick={(event) => {
-              event.stopPropagation();
-              onRemove(item.id);
-            }}
-          >
-            <svg
-              viewBox="0 0 24 24"
-              width="20"
-              height="20"
-              fill="none"
-              stroke="currentColor"
+          <div className="aa-ItemActions">
+            <button
+              className="aa-ItemActionButton"
+              title="Remove this search"
+              onClick={(event) => {
+                event.stopPropagation();
+                onRemove(item.id);
+              }}
             >
-              <path
-                d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-              />
-            </svg>
-          </button>
+              <svg
+                viewBox="0 0 24 24"
+                width="18"
+                height="18"
+                fill="none"
+                stroke="currentColor"
+              >
+                <path
+                  d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                />
+              </svg>
+            </button>
+          </div>
         </Fragment>
       );
     },
