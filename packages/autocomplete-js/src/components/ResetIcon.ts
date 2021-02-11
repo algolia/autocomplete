af43a379215376cab811adfd512bc7fb60965803
Kevin Granger <shipowlata@gmail.com>
2021-02-11 10:09:19 +0100

    fix(theme): update icons and Detached mode design (#443)

--- packages/autocomplete-js/src/components/ResetIcon.ts
+++ packages/autocomplete-js/src/components/ResetIcon.ts
@@ -3,18 +3,16 @@ import { Component } from '../types/Component';
 export const ResetIcon: Component<{}, SVGSVGElement> = () => {
   const element = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
   element.setAttribute('class', 'aa-ResetIcon');
-  element.setAttribute('viewBox', '0 0 20 20');
-  element.setAttribute('width', '20');
-  element.setAttribute('height', '20');
+  element.setAttribute('viewBox', '0 0 24 24');
+  element.setAttribute('width', '18');
+  element.setAttribute('height', '18');
   element.setAttribute('fill', 'currentColor');
 
   const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
   path.setAttribute(
     'd',
-    'M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z'
+    'M5.293 6.707l5.293 5.293-5.293 5.293c-0.391 0.391-0.391 1.024 0 1.414s1.024 0.391 1.414 0l5.293-5.293 5.293 5.293c0.391 0.391 1.024 0.391 1.414 0s0.391-1.024 0-1.414l-5.293-5.293 5.293-5.293c0.391-0.391 0.391-1.024 0-1.414s-1.024-0.391-1.414 0l-5.293 5.293-5.293-5.293c-0.391-0.391-1.024-0.391-1.414 0s-0.391 1.024 0 1.414z'
   );
-  path.setAttribute('fill-rule', 'evenodd');
-  path.setAttribute('clip-rule', 'evenodd');
 
   element.appendChild(path);
 
