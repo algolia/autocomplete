65b5f4375af41a3670711915ab8a6c23f17df4e9
François Chalifour <francois.chalifour@gmail.com>
2021-02-15 15:55:30 +0100

    test(js): test highlight features

--- packages/autocomplete-js/src/__tests__/highlightHit.test.ts
+++ packages/autocomplete-js/src/__tests__/highlightHit.test.ts
@@ -1,3 +1,86 @@
+import { highlightHit } from '../highlight';
+
 describe('highlightHit', () => {
-  test.todo('tests');
+  test('returns a highlighted hit', () => {
+    expect(
+      highlightHit({
+        hit: {
+          objectID: 'amazon fire tablets',
+          query: 'amazon fire tablets',
+          _highlightResult: {
+            query: {
+              fullyHighlighted: false,
+              matchLevel: 'full',
+              matchedWords: ['fire', 'tablet'],
+              value:
+                'amazon __aa-highlight__fire__/aa-highlight__ __aa-highlight__tablet__/aa-highlight__s',
+            },
+          },
+        },
+        attribute: 'query',
+      })
+    ).toEqual([
+      'amazon ',
+      expect.objectContaining({
+        type: 'mark',
+        props: {
+          children: 'fire',
+        },
+      }),
+      ' ',
+      expect.objectContaining({
+        type: 'mark',
+        props: {
+          children: 'tablet',
+        },
+      }),
+      's',
+    ]);
+  });
+
+  test('accepts custom createElement', () => {
+    expect(
+      highlightHit({
+        hit: {
+          objectID: 'amazon fire tablets',
+          query: 'amazon fire tablets',
+          _highlightResult: {
+            query: {
+              fullyHighlighted: false,
+              matchLevel: 'full',
+              matchedWords: ['fire', 'tablet'],
+              value:
+                'amazon __aa-highlight__fire__/aa-highlight__ __aa-highlight__tablet__/aa-highlight__s',
+            },
+          },
+        },
+        attribute: 'query',
+        createElement(type, props, children) {
+          return {
+            type,
+            props,
+            children,
+          };
+        },
+      })
+    ).toEqual([
+      'amazon ',
+      expect.objectContaining({
+        type: 'mark',
+        props: {
+          key: 1,
+        },
+        children: 'fire',
+      }),
+      ' ',
+      expect.objectContaining({
+        type: 'mark',
+        props: {
+          key: 3,
+        },
+        children: 'tablet',
+      }),
+      's',
+    ]);
+  });
 });
