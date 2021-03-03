10b7c9e3f1555f9423bf79b7f2ff3ba2602ae542
yiwang <73939465+yixuan-wang20@users.noreply.github.com>
2021-03-03 16:14:46 +0100

    docs(sandbox): add Query Suggestions Only  (#464)
    * qs-only
    
    * qs-only
    
    * qs-only
    
    * modify name in json file

---
+++ examples/query-suggestions/app.tsx
@@ -0,0 +1,26 @@
+import { autocomplete } from '@algolia/autocomplete-js';
+import { createQuerySuggestionsPlugin } from '@algolia/autocomplete-plugin-query-suggestions';
+import algoliasearch from 'algoliasearch';
+
+import '@algolia/autocomplete-theme-classic';
+
+const appId = 'latency';
+const apiKey = '6be0576ff61c053d5f9a3225e2a90f76';
+const searchClient = algoliasearch(appId, apiKey);
+
+const querySuggestionsPlugin = createQuerySuggestionsPlugin({
+  searchClient,
+  indexName: 'instant_search_demo_query_suggestions',
+  getSearchParams() {
+    return {
+      hitsPerPage: 10,
+    };
+  },
+});
+
+autocomplete({
+  container: '#autocomplete',
+  placeholder: 'Search',
+  openOnFocus: true,
+  plugins: [querySuggestionsPlugin],
+});
