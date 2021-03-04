3c42c9b533d7de6dd67862d9313e42c5c66846e4
yixuan WANG <yixuan.wang@algolia.com>
2021-03-04 01:24:39 +0100

    docs(sandbox): add Multiple Datasets with Headers 

---
+++ examples/multiple-datasets-with-headers/app.tsx
@@ -0,0 +1,75 @@
+/** @jsx h */
+import { autocomplete } from '@algolia/autocomplete-js';
+import { createQuerySuggestionsPlugin } from '@algolia/autocomplete-plugin-query-suggestions';
+import { createLocalStorageRecentSearchesPlugin } from '@algolia/autocomplete-plugin-recent-searches';
+import algoliasearch from 'algoliasearch';
+import { h, Fragment } from 'preact';
+
+import '@algolia/autocomplete-theme-classic';
+import { createCategoriesPlugin } from './categoriesPlugin';
+
+const appId = 'latency';
+const apiKey = '6be0576ff61c053d5f9a3225e2a90f76';
+const searchClient = algoliasearch(appId, apiKey);
+
+const recentSearchesPlugin = createLocalStorageRecentSearchesPlugin({
+  key: 'qs-with-rs-example',
+  limit: 3,
+  transformSource({ source }) {
+    return {
+      ...source,
+      templates: {
+        ...source.templates,
+        header({ items, state }) {
+          if (Boolean(state.query) || items.length === 0) {
+            return null;
+          }
+
+          return (
+            <Fragment>
+              <span className="aa-SourceHeaderTitle">Your searches</span>
+              <div className="aa-SourceHeaderLine" />
+            </Fragment>
+          );
+        },
+      },
+    };
+  },
+});
+const querySuggestionsPlugin = createQuerySuggestionsPlugin({
+  searchClient,
+  indexName: 'instant_search_demo_query_suggestions',
+  getSearchParams() {
+    return recentSearchesPlugin.data.getAlgoliaSearchParams({
+      hitsPerPage: 5,
+    });
+  },
+  transformSource({ source }) {
+    return {
+      ...source,
+      templates: {
+        ...source.templates,
+        header({ items, state }) {
+          if (Boolean(state.query) || items.length === 0) {
+            return null;
+          }
+
+          return (
+            <Fragment>
+              <span className="aa-SourceHeaderTitle">Popular searches</span>
+              <div className="aa-SourceHeaderLine" />
+            </Fragment>
+          );
+        },
+      },
+    };
+  },
+});
+
+const categoriesPlugin = createCategoriesPlugin({ searchClient });
+autocomplete({
+  container: '#autocomplete',
+  placeholder: 'Search',
+  openOnFocus: true,
+  plugins: [recentSearchesPlugin, querySuggestionsPlugin, categoriesPlugin],
+});
