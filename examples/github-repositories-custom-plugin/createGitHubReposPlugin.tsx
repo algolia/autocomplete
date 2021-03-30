0eb0f1024e63c87da20a71f2c384b703676e1ee0
Sarah Dayan <sarah.dayan@algolia.com>
2021-03-30 16:47:56 +0200

    fix: remove redundant word and add an accessible label

--- examples/github-repositories-custom-plugin/createGitHubReposPlugin.tsx
+++ examples/github-repositories-custom-plugin/createGitHubReposPlugin.tsx
@@ -72,6 +72,7 @@ export function createGitHubReposPlugin(
                               }}
                             >
                               <svg
+                                aria-label={`${stars} stars`}
                                 style={{
                                   position: 'relative',
                                   top: '1px',
@@ -85,12 +86,13 @@ export function createGitHubReposPlugin(
                                 <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
                               </svg>{' '}
                               <span
+                                aria-hidden="true"
                                 style={{
                                   fontSize: '0.8em',
                                   color: 'var(--aa-content-text-color)',
                                 }}
                               >
-                                {stars} stars
+                                {stars}
                               </span>
                             </div>
                           </div>
