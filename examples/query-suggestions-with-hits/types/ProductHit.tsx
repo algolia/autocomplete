849c269460798679b58e41cb33f6e24437668bdb
shortcuts <vannicattec@gmail.com>
2021-04-15 17:33:19 +0200

    Remove uneeded types in example/qs-with-hits

--- examples/query-suggestions-with-hits/types/ProductHit.tsx
+++ examples/query-suggestions-with-hits/types/ProductHit.tsx
@@ -3,28 +3,11 @@ import { Hit } from '@algolia/client-search';
 type ProductRecord = {
   brand: string;
   categories: string[];
-  comments: number;
   description: string;
-  free_shipping: boolean;
-  hierarchicalCategories: {
-    lvl0: string;
-    lvl1?: string;
-    lvl2?: string;
-    lvl3?: string;
-    lvl4?: string;
-    lvl5?: string;
-    lvl6?: string;
-  };
   image: string;
   name: string;
-  popularity: number;
   price: number;
-  prince_range: string;
   rating: number;
-  sale: boolean;
-  sale_price: string;
-  type: string;
-  url: string;
 };
 
 type WithAutocompleteAnalytics<THit> = THit & {
