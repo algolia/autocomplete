77f3931933e0ea249d29a7bfdcbf6a472d1ee94a
Sarah Dayan <sarah.dayan@algolia.com>
2021-04-15 10:39:58 +0200

    chore: remove comment

--- packages/autocomplete-js/src/__tests__/requester.test.ts
+++ packages/autocomplete-js/src/__tests__/requester.test.ts
@@ -57,7 +57,6 @@ describe('requester', () => {
       panelContainer,
       getSources({ query }) {
         // @TODO: change order to ensure proper distribution
-        // @TODO: add static source
         return [
           {
             sourceId: 'custom',
