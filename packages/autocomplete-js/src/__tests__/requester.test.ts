407a7ccc6b134a46b65642afed3bf4ee9a457da0
Sarah Dayan <sarah.dayan@algolia.com>
2021-04-15 10:27:32 +0200

    tests: initialize search clients locally

--- packages/autocomplete-js/src/__tests__/requester.test.ts
+++ packages/autocomplete-js/src/__tests__/requester.test.ts
@@ -7,45 +7,6 @@ import {
   createSearchClient,
 } from '../../../../test/utils';
 
-// @TODO: instantiate locally
-const searchClient = createSearchClient({
-  search: jest.fn(() =>
-    Promise.resolve(
-      createMultiSearchResponse<{ label: string }>(
-        { hits: [{ objectID: '1', label: 'Hit 1' }] },
-        { facetHits: [{ count: 2, value: 'Hit 2' }] },
-        { hits: [{ objectID: '3', label: 'Hit 3' }] },
-        { hits: [{ objectID: '4', label: 'Hit 4' }] },
-        { hits: [{ objectID: '5', label: 'Hit 5' }] },
-        { facetHits: [{ count: 6, value: 'Hit 6' }] }
-      )
-    )
-  ),
-});
-
-const searchClient2 = createSearchClient({
-  search: jest.fn(() =>
-    Promise.resolve(
-      createMultiSearchResponse<{ label: string }>({
-        hits: [{ objectID: '7', label: 'Hit 7' }],
-      })
-    )
-  ),
-});
-
-const searchClient3 = createSearchClient({
-  search: jest.fn(() =>
-    Promise.resolve(
-      createMultiSearchResponse<{ label: string }>(
-        {
-          hits: [{ objectID: '1', label: 'Hit 1' }],
-        },
-        { facetHits: [{ count: 2, value: 'Hit 2' }] }
-      )
-    )
-  ),
-});
-
 const customFetch = jest.fn(() =>
   Promise.resolve([
     {
@@ -67,6 +28,32 @@ describe('requester', () => {
     const panelContainer = document.createElement('div');
 
     document.body.appendChild(panelContainer);
+
+    const searchClient = createSearchClient({
+      search: jest.fn(() =>
+        Promise.resolve(
+          createMultiSearchResponse<{ label: string }>(
+            { hits: [{ objectID: '1', label: 'Hit 1' }] },
+            { facetHits: [{ count: 2, value: 'Hit 2' }] },
+            { hits: [{ objectID: '3', label: 'Hit 3' }] },
+            { hits: [{ objectID: '4', label: 'Hit 4' }] },
+            { hits: [{ objectID: '5', label: 'Hit 5' }] },
+            { facetHits: [{ count: 6, value: 'Hit 6' }] }
+          )
+        )
+      ),
+    });
+
+    const searchClient2 = createSearchClient({
+      search: jest.fn(() =>
+        Promise.resolve(
+          createMultiSearchResponse<{ label: string }>({
+            hits: [{ objectID: '7', label: 'Hit 7' }],
+          })
+        )
+      ),
+    });
+
     autocomplete({
       container,
       panelContainer,
@@ -337,6 +324,20 @@ describe('requester', () => {
     const panelContainer = document.createElement('div');
 
     document.body.appendChild(panelContainer);
+
+    const searchClient = createSearchClient({
+      search: jest.fn(() =>
+        Promise.resolve(
+          createMultiSearchResponse<{ label: string }>(
+            {
+              hits: [{ objectID: '1', label: 'Hit 1' }],
+            },
+            { facetHits: [{ count: 2, value: 'Hit 2' }] }
+          )
+        )
+      ),
+    });
+
     autocomplete({
       container,
       panelContainer,
@@ -346,7 +347,7 @@ describe('requester', () => {
             sourceId: 'hits',
             getItems() {
               return getAlgoliaResults({
-                searchClient: searchClient3,
+                searchClient,
                 queries: [
                   {
                     indexName: 'indexName',
@@ -371,7 +372,7 @@ describe('requester', () => {
             sourceId: 'facets',
             getItems() {
               return getAlgoliaFacets({
-                searchClient: searchClient3,
+                searchClient,
                 queries: [
                   {
                     indexName: 'indexName',
