37aeac418f426a39236305bbfc3e5bb639acb95d
François Chalifour <francois.chalifour@gmail.com>
2021-04-19 22:58:34 +0200

    docs(examples): add Voice Search example

---
+++ examples/voice-search/voiceSearchApi.ts
@@ -0,0 +1,139 @@
+export type VoiceSearchStatus =
+  | 'initial'
+  | 'askingPermission'
+  | 'waiting'
+  | 'recognizing'
+  | 'finished'
+  | 'error';
+
+type VoiceSearchState = {
+  status: VoiceSearchStatus;
+  transcript: string;
+  isSpeechFinal: boolean;
+  errorCode?: string;
+};
+
+type CreateVoiceSearchParams = {
+  searchAsYouSpeak: boolean;
+  language?: string;
+  onQueryChange(query: string): void;
+  onStateChange(state: VoiceSearchState): void;
+};
+
+type VoiceSearchApi = {
+  isBrowserSupported(): boolean;
+  startListening(): void;
+  stopListening(): void;
+  destroy(): void;
+};
+
+function createState(state: Partial<VoiceSearchState>): VoiceSearchState {
+  return {
+    status: 'initial',
+    transcript: '',
+    isSpeechFinal: false,
+    errorCode: undefined,
+    ...state,
+  };
+}
+
+export function createVoiceSearch({
+  searchAsYouSpeak,
+  language,
+  onQueryChange,
+  onStateChange,
+}: CreateVoiceSearchParams): VoiceSearchApi {
+  const SpeechRecognitionAPI: new () => SpeechRecognition =
+    (window as any).webkitSpeechRecognition ||
+    (window as any).SpeechRecognition;
+  let state: VoiceSearchState = createState({});
+  let recognition: SpeechRecognition | undefined;
+
+  function isBrowserSupported() {
+    return Boolean(SpeechRecognitionAPI);
+  }
+
+  function setState(newState: Partial<VoiceSearchState> = {}) {
+    state = { ...state, ...newState };
+    onStateChange(state);
+  }
+
+  function onStart() {
+    setState({ status: 'waiting' });
+  }
+
+  function onError(event: SpeechRecognitionErrorEvent) {
+    setState({ status: 'error', errorCode: event.error });
+  }
+
+  function onResult(event: SpeechRecognitionEvent) {
+    setState({
+      status: 'recognizing',
+      transcript:
+        (event.results[0] &&
+          event.results[0][0] &&
+          event.results[0][0].transcript) ||
+        '',
+      isSpeechFinal: event.results[0] && event.results[0].isFinal,
+    });
+    if (searchAsYouSpeak && state.transcript) {
+      onQueryChange(state.transcript);
+    }
+  }
+
+  function onEnd() {
+    if (!state.errorCode && state.transcript && !searchAsYouSpeak) {
+      onQueryChange(state.transcript);
+    }
+
+    if (state.status !== 'error') {
+      setState({ status: 'finished' });
+    }
+  }
+
+  function startListening() {
+    recognition = new SpeechRecognitionAPI();
+    if (!recognition) {
+      return;
+    }
+
+    setState(createState({ status: 'askingPermission' }));
+    recognition.interimResults = true;
+    if (language) {
+      recognition.lang = language;
+    }
+    recognition.addEventListener('start', onStart);
+    recognition.addEventListener('error', onError);
+    recognition.addEventListener('result', onResult);
+    recognition.addEventListener('end', onEnd);
+    recognition.start();
+  }
+
+  function destroy() {
+    if (!recognition) {
+      return;
+    }
+
+    recognition.stop();
+    recognition.removeEventListener('start', onStart);
+    recognition.removeEventListener('error', onError);
+    recognition.removeEventListener('result', onResult);
+    recognition.removeEventListener('end', onEnd);
+    recognition = undefined;
+  }
+
+  function stopListening() {
+    destroy();
+    // Because `destroy` removes event listeners, `end` listener is not called.
+    // So we're setting the `status` as `finished` here.
+    // If we don't do it, it will be still `waiting` or `recognizing`.
+    setState(createState({ status: 'finished' }));
+  }
+
+  return {
+    isBrowserSupported,
+    startListening,
+    stopListening,
+    destroy,
+  };
+}
---
+++ examples/voice-search/voiceSearchPlugin.tsx
@@ -0,0 +1,119 @@
+/** @jsx h */
+import { AutocompletePlugin } from '@algolia/autocomplete-js';
+import { h, render } from 'preact';
+
+import { createVoiceSearch, VoiceSearchStatus } from './voiceSearchApi';
+
+export function createVoiceSearchPlugin(): AutocompletePlugin<any, undefined> {
+  return {
+    subscribe({ setQuery, setIsOpen, refresh }) {
+      requestAnimationFrame(() => {
+        const inputWrapperSuffix = document.querySelector(
+          '.aa-InputWrapperSuffix'
+        );
+
+        if (!inputWrapperSuffix) {
+          return;
+        }
+
+        const input = document.querySelector<HTMLInputElement>('.aa-Input');
+        const voiceSearchOverlayContainer = document.createElement('div');
+
+        const voiceSearch = createVoiceSearch({
+          searchAsYouSpeak: false,
+          onQueryChange(query) {
+            setQuery(query);
+            setIsOpen(true);
+            refresh();
+            requestAnimationFrame(() => {
+              input.focus();
+            });
+          },
+          onStateChange(state) {
+            if (state.status === 'finished' || state.status === 'error') {
+              document.body.removeChild(voiceSearchOverlayContainer);
+            } else {
+              render(
+                <VoiceSearchOverlay
+                  status={state.status}
+                  transcript={state.transcript}
+                />,
+                voiceSearchOverlayContainer
+              );
+            }
+          },
+        });
+
+        if (!voiceSearch.isBrowserSupported()) {
+          return;
+        }
+
+        const voiceSearchContainer = document.createElement('div');
+        voiceSearchContainer.classList.add('aa-VoiceSearch');
+        const voiceSearchButton = document.createElement('button');
+        voiceSearchButton.setAttribute('title', 'Voice Search');
+        voiceSearchButton.classList.add('aa-VoiceSearchButton');
+
+        render(<VoiceSearchIcon />, voiceSearchButton);
+        voiceSearchContainer.appendChild(voiceSearchButton);
+        inputWrapperSuffix.appendChild(voiceSearchContainer);
+
+        voiceSearchOverlayContainer.classList.add('aa-VoiceSearchOverlay');
+
+        voiceSearchButton.addEventListener('click', (event) => {
+          event.preventDefault();
+          voiceSearch.startListening();
+          document.body.appendChild(voiceSearchOverlayContainer);
+        });
+      });
+    },
+    getSources() {
+      return [];
+    },
+  };
+}
+
+function VoiceSearchIcon() {
+  return (
+    <svg fill="currentColor" viewBox="0 0 24 24">
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M7 4a3 3 0 016 0v4a3 3 0 11-6 0V4zm4 10.93A7.001 7.001 0 0017 8a1 1 0 10-2 0A5 5 0 015 8a1 1 0 00-2 0 7.001 7.001 0 006 6.93V17H6a1 1 0 100 2h8a1 1 0 100-2h-3v-2.07z"
+      />
+    </svg>
+  );
+}
+
+function getStatusText(status: VoiceSearchStatus) {
+  switch (status) {
+    case 'waiting':
+      return 'Listening...';
+    case 'recognizing':
+      return 'Recognizing...';
+    default:
+      return status;
+  }
+}
+
+function VoiceSearchOverlay({ status, transcript }) {
+  return (
+    <div className="aa-VoiceSearchWrapper">
+      <div>{transcript || getStatusText(status)}</div>
+      <svg
+        width="48"
+        height="48"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="{2}"
+          d="M19 11a7 7 0 01-7 7m0 0a7 7 0 01-7-7m7 7v4m0 0H8m4 0h4m-4-8a3 3 0 01-3-3V5a3 3 0 116 0v6a3 3 0 01-3 3z"
+        ></path>
+      </svg>
+    </div>
+  );
+}
