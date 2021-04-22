5a0d778b7517465958fd3a50a1bc82bb9792ceed
Sarah Dayan <sarah.dayan@algolia.com>
2021-04-22 18:28:48 +0200

    test(autocomplete-js): add unit test for getAlgoliaResults

---
+++ packages/autocomplete-js/src/requesters/__tests__/getAlgoliaResults.test.ts
@@ -0,0 +1,82 @@
+import { createSearchClient } from '../../../../../test/utils';
+import { getAlgoliaResults } from '../getAlgoliaResults';
+
+describe('getAlgoliaResults', () => {
+  test('returns the description', () => {
+    const searchClient = createSearchClient({
+      search: jest.fn(),
+    });
+    const description = getAlgoliaResults({
+      searchClient,
+      queries: [
+        {
+          indexName: 'indexName',
+          query: 'query',
+        },
+        {
+          indexName: 'indexName2',
+          query: 'query',
+        },
+      ],
+    });
+
+    expect(description).toEqual({
+      execute: expect.any(Function),
+      transformResponse: expect.any(Function),
+      searchClient,
+      queries: [
+        {
+          indexName: 'indexName',
+          query: 'query',
+        },
+        {
+          indexName: 'indexName2',
+          query: 'query',
+        },
+      ],
+    });
+  });
+
+  test('defaults transformItems to retrieve hits', async () => {
+    const searchClient = createSearchClient({
+      search: jest.fn(),
+    });
+    const description = getAlgoliaResults<{ label: string }>({
+      searchClient,
+      queries: [
+        {
+          indexName: 'indexName',
+          query: 'query',
+        },
+      ],
+    });
+
+    const transformedResponse = description.transformResponse({
+      results: [],
+      hits: [
+        [
+          {
+            objectID: '1',
+            label: 'Label',
+            _highlightResult: {
+              label: { value: 'Label', matchLevel: 'none', matchedWords: [] },
+            },
+          },
+        ],
+      ],
+      facetHits: [],
+    });
+
+    expect(transformedResponse).toEqual([
+      [
+        {
+          objectID: '1',
+          label: 'Label',
+          _highlightResult: {
+            label: { value: 'Label', matchLevel: 'none', matchedWords: [] },
+          },
+        },
+      ],
+    ]);
+  });
+});
