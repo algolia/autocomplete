bce70b7216b37b069ff48a4c864fccc36dfe3d80
François Chalifour <francois.chalifour@gmail.com>
2021-04-22 09:29:08 +0200

    test(getAlgoliaResults): test default `transformResponse`

--- packages/autocomplete-core/src/requesters/__tests__/getAlgoliaResults.test.ts
+++ packages/autocomplete-core/src/requesters/__tests__/getAlgoliaResults.test.ts
@@ -36,4 +36,47 @@ describe('getAlgoliaResults', () => {
       ],
     });
   });
+
+  test('defaults transformItems to retrieve hits', async () => {
+    const searchClient = createSearchClient({
+      search: jest.fn(),
+    });
+    const description = await getAlgoliaResults<{ label: string }>({
+      searchClient,
+      queries: [
+        {
+          indexName: 'indexName',
+          query: 'query',
+        },
+      ],
+    });
+
+    const transformedResponse = description.transformResponse({
+      results: [],
+      hits: [
+        [
+          {
+            objectID: '1',
+            label: 'Label',
+            _highlightResult: {
+              label: { value: 'Label', matchLevel: 'none', matchedWords: [] },
+            },
+          },
+        ],
+      ],
+      facetHits: [],
+    });
+
+    expect(transformedResponse).toEqual([
+      [
+        {
+          objectID: '1',
+          label: 'Label',
+          _highlightResult: {
+            label: { value: 'Label', matchLevel: 'none', matchedWords: [] },
+          },
+        },
+      ],
+    ]);
+  });
 });
