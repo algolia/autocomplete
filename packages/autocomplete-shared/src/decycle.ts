1c63db7964eedd901a088e36dcf7ef10c3509d6d
Sarah Dayan <5370675+sarahdayan@users.noreply.github.com>
2021-07-21 18:36:54 +0200

    refactor: use Set instead of array for faster access

--- packages/autocomplete-shared/src/decycle.ts
+++ packages/autocomplete-shared/src/decycle.ts
@@ -2,16 +2,16 @@
  * Decycles objects with circular references.
  * This is used to print cyclic structures in development environment only.
  */
-export function decycle(obj: any, seen: any[] = []) {
+export function decycle(obj: any, seen = new Set()) {
   if (!__DEV__ || !obj || typeof obj !== 'object') {
     return obj;
   }
 
-  if (seen.includes(obj)) {
+  if (seen.has(obj)) {
     return '[Circular]';
   }
 
-  const newSeen = seen.concat([obj]);
+  const newSeen = seen.add(obj);
 
   if (Array.isArray(obj)) {
     return obj.map((x) => decycle(x, newSeen));
