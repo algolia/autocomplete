5e02c18fd5e2dd947742e57f7b36ba894e0f7a57
Sarah Dayan <5370675+sarahdayan@users.noreply.github.com>
2021-07-21 12:05:29 +0200

    build: replace node-sass with dart-sass (#633)

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -2,7 +2,7 @@ import autoprefixer from 'autoprefixer';
 import cssnano from 'cssnano';
 import color from 'postcss-color-rgb';
 import comment from 'postcss-comment';
-import sass from 'postcss-node-sass';
+import sass from '@csstools/postcss-sass';
 import presetEnv from 'postcss-preset-env';
 
 export default {
