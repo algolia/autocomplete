69e26caa21e2de8985941233695c80042025e326
Clément Vannicatte <vannicattec@gmail.com>
2021-07-23 16:10:35 +0200

    feat(theme): provide non-minified theme

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,21 +1,23 @@
+import sass from '@csstools/postcss-sass';
 import autoprefixer from 'autoprefixer';
 import cssnano from 'cssnano';
 import color from 'postcss-color-rgb';
 import comment from 'postcss-comment';
-import sass from '@csstools/postcss-sass';
 import presetEnv from 'postcss-preset-env';
+
+const MINIFIED = process.env.MINIFIED;
+const plugins = [
+  presetEnv({
+    features: {
+      'nesting-rules': false,
+    },
+  }),
+  color,
+  sass,
+  autoprefixer,
+];
 
 export default {
   parser: comment,
-  plugins: [
-    presetEnv({
-      features: {
-        'nesting-rules': false,
-      },
-    }),
-    color,
-    sass,
-    autoprefixer,
-    cssnano,
-  ],
+  plugins: MINIFIED ? [...plugins, cssnano] : plugins,
 };
