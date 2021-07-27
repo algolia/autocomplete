e92d8b8652f41e70a382614f248ad6da0691e923
François Chalifour <francoischalifour@users.noreply.github.com>
2021-07-27 16:14:17 +0200

    docs(examples): add React InstantSearch example (#642)

---
+++ examples/react-instantsearch/src/Autocomplete.js
@@ -0,0 +1,30 @@
+/** @jsx React.createElement */
+
+import { autocomplete } from '@algolia/autocomplete-js';
+import React, { createElement, Fragment, useEffect, useRef } from 'react';
+import { render } from 'react-dom';
+
+export function Autocomplete(props) {
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    if (!containerRef.current) {
+      return undefined;
+    }
+
+    const search = autocomplete({
+      container: containerRef.current,
+      renderer: { createElement, Fragment },
+      render({ children }, root) {
+        render(children, root);
+      },
+      ...props,
+    });
+
+    return () => {
+      search.destroy();
+    };
+  }, []);
+
+  return <div ref={containerRef} />;
+}
