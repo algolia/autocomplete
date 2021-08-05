42cc8ef57f7be9101429ffc34037140618c03e94
Sarah Dayan <5370675+sarahdayan@users.noreply.github.com>
2021-08-05 23:35:28 +0200

    chore: use better icons

--- packages/autocomplete-plugin-tags/src/getTemplates.tsx
+++ packages/autocomplete-plugin-tags/src/getTemplates.tsx
@@ -10,13 +10,15 @@ export function getTemplates<TTag>(): SourceTemplates<Tag<TTag>> {
         <div className="aa-ItemWrapper">
           <div className="aa-ItemContent">
             <div className="aa-ItemIcon aa-ItemIcon--noBorder">
-              <svg fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994 1.994 0 013 12V7a4 4 0 014-4z"
-                />
+              <svg
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth={2}
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="M20.59 13.41l-7.17 7.17a2 2 0 01-2.83 0L2 12V2h10l8.59 8.59a2 2 0 010 2.82zM7 7h.01" />
               </svg>
             </div>
             <div className="aa-ItemContentBody">
