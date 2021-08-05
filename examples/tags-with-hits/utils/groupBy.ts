bc59f23c536987575e489c8b583ff6ae0da764d8
Sarah Dayan <5370675+sarahdayan@users.noreply.github.com>
2021-08-06 00:44:12 +0200

    docs(examples): add Tags with hits example

---
+++ examples/tags-with-hits/utils/groupBy.ts
@@ -0,0 +1,16 @@
+export function groupBy<TItem>(
+  items: TItem[],
+  predicate: (item: TItem) => string
+) {
+  return items.reduce((acc, item) => {
+    const key = predicate(item);
+
+    if (!acc.hasOwnProperty(key)) {
+      acc[key] = [];
+    }
+
+    acc[key].push(item);
+
+    return acc;
+  }, {});
+}
