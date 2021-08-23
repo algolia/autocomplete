fd7a9ed51d7a977d0fe6007e28b19f1d568b1cbb
Sarah Dayan <5370675+sarahdayan@users.noreply.github.com>
2021-08-23 16:41:45 +0200

    docs: add TSDoc

--- packages/autocomplete-plugin-tags/src/createTagsPlugin.ts
+++ packages/autocomplete-plugin-tags/src/createTagsPlugin.ts
@@ -13,24 +13,59 @@ type OnChangeParams<TTag> = PluginSubscribeParams<any> &
   OnTagsChangeParams<TTag>;
 
 type TagsPluginData<TTag> = {
+  /**
+   * Returns the current list of tags.
+   *
+   * @link https://www.algolia.com/doc/ui-libraries/autocomplete/api-reference/autocomplete-plugin-tags/createtagsplugin/#param-tags
+   */
   readonly tags: Array<Tag<TTag>>;
-  setTags: (tags: Array<BaseTag<TTag>>) => void;
+  /**
+   * Adds tags to the list.
+   *
+   * @link https://www.algolia.com/doc/ui-libraries/autocomplete/api-reference/autocomplete-plugin-tags/createtagsplugin/#param-addtags
+   */
   addTags: (tags: Array<BaseTag<TTag>>) => void;
+  /**
+   * Sets the list of tags.
+   *
+   * @link https://www.algolia.com/doc/ui-libraries/autocomplete/api-reference/autocomplete-plugin-tags/createtagsplugin/#param-settags
+   */
+  setTags: (tags: Array<BaseTag<TTag>>) => void;
 };
 
 export type CreateTagsPluginParams<
   TTag extends Record<string, any>,
   TItem extends BaseItem
 > = {
+  /**
+   * A set of initial tags to pass to the plugin.
+   *
+   * @link https://www.algolia.com/doc/ui-libraries/autocomplete/api-reference/autocomplete-plugin-tags/createtagsplugin/#param-initialtags
+   */
   initialTags?: Array<BaseTag<TTag>>;
+  /**
+   * A function to specify what sources the plugin should subscribe to. The plugin adds a tag when selecting an item from these sources.
+   *
+   * @link https://www.algolia.com/doc/ui-libraries/autocomplete/api-reference/autocomplete-plugin-tags/createtagsplugin/#param-gettagssubscribers
+   */
   getTagsSubscribers?(): Array<{
     sourceId: string;
     getTag(params: { item: TItem }): BaseTag<TTag>;
   }>;
+  /**
+   * A function to transform the returned tags source.
+   *
+   * @link https://www.algolia.com/doc/ui-libraries/autocomplete/api-reference/autocomplete-plugin-tags/createtagsplugin/#param-transformsource
+   */
   transformSource?(params: {
     source: AutocompleteSource<Tag<TTag>>;
     state: AutocompleteState<Tag<TTag>>;
   }): AutocompleteSource<Tag<TTag>> | undefined;
+  /**
+   * A function called when the list of tags changes.
+   *
+   * @link https://www.algolia.com/doc/ui-libraries/autocomplete/api-reference/autocomplete-plugin-tags/createtagsplugin/#param-onchange
+   */
   onChange?(params: OnChangeParams<TTag>): void;
 };
 
