101cf6b53c14c660ba89b4b93b1a74e534ba1115
Sarah Dayan <5370675+sarahdayan@users.noreply.github.com>
2021-08-23 17:42:06 +0200

    test: bootstrap test suite

---
+++ packages/autocomplete-plugin-tags/src/__tests__/createTagsPlugin.test.ts
@@ -0,0 +1,23 @@
+describe('createTagsPlugin', () => {
+  test.todo('adds a tags source');
+
+  test.todo('exposes an API');
+
+  test.todo('returns the tags');
+
+  test.todo('adds new tags');
+
+  test.todo('replaces tags');
+
+  test.todo('runs a callback when tags change');
+
+  test.todo('adds a tag on subscriber item select');
+
+  test.todo('removes a tag on tags source item select');
+
+  test.todo('transforms the tags source');
+
+  test.todo('removes the tags source');
+
+  test.todo('keeps the panel open on tags update');
+});
