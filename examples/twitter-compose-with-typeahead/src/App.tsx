05c184cd4907db8c5d92cbc0c77644bfe18653a2
Sarah Dayan <5370675+sarahdayan@users.noreply.github.com>
2021-09-17 18:53:23 +0200

    chore: disable debug mode

--- examples/twitter-compose-with-typeahead/src/App.tsx
+++ examples/twitter-compose-with-typeahead/src/App.tsx
@@ -7,11 +7,7 @@ import './App.css';
 export function App() {
   return (
     <div className="container">
-      <Autocomplete
-        placeholder="What's up?"
-        defaultActiveItemId={0}
-        debug={true}
-      />
+      <Autocomplete placeholder="What's up?" defaultActiveItemId={0} />
     </div>
   );
 }
