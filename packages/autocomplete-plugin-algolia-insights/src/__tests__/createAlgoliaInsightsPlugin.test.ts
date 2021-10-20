09519e6b15d89f0e1670c24228920962eaeb2c10
Sarah Dayan <5370675+sarahdayan@users.noreply.github.com>
2021-10-21 01:59:38 +0200

    test(plugins): test Algolia Insights plugin

--- packages/autocomplete-plugin-algolia-insights/src/__tests__/createAlgoliaInsightsPlugin.test.ts
+++ packages/autocomplete-plugin-algolia-insights/src/__tests__/createAlgoliaInsightsPlugin.test.ts
@@ -1,6 +1,8 @@
+import { createAutocomplete } from '@algolia/autocomplete-core';
 import { noop } from '@algolia/autocomplete-shared';
 import insightsClient from 'search-insights';
 
+import { createPlayground } from '../../../../test/utils';
 import { createAlgoliaInsightsPlugin } from '../createAlgoliaInsightsPlugin';
 
 describe('createAlgoliaInsightsPlugin', () => {
@@ -22,5 +24,62 @@ describe('createAlgoliaInsightsPlugin', () => {
     });
   });
 
-  test.todo('tests');
+  test('exposes the Search Insights API on the context', () => {
+    const insightsPlugin = createAlgoliaInsightsPlugin({ insightsClient });
+    const onStateChange = jest.fn();
+
+    createPlayground(createAutocomplete, {
+      onStateChange,
+      plugins: [insightsPlugin],
+    });
+
+    expect(onStateChange).toHaveBeenLastCalledWith(
+      expect.objectContaining({
+        state: expect.objectContaining({
+          context: expect.objectContaining({
+            algoliaInsightsPlugin: expect.objectContaining({
+              insights: expect.objectContaining({
+                init: expect.any(Function),
+                setUserToken: expect.any(Function),
+                clickedObjectIDsAfterSearch: expect.any(Function),
+                clickedObjectIDs: expect.any(Function),
+                clickedFilters: expect.any(Function),
+                convertedObjectIDsAfterSearch: expect.any(Function),
+                convertedObjectIDs: expect.any(Function),
+                convertedFilters: expect.any(Function),
+                viewedObjectIDs: expect.any(Function),
+                viewedFilters: expect.any(Function),
+              }),
+            }),
+          }),
+        }),
+      })
+    );
+  });
+
+  describe('onItemsChange', () => {
+    test.todo('sends a `viewedObjectIDs` event by default');
+
+    test.todo('sends a custom event');
+
+    test.todo('debounces calls');
+
+    test.todo('does not send an event with non-Algolia Insights hits');
+  });
+
+  describe('onSelect', () => {
+    test.todo('sends a `clickedObjectIDsAfterSearch` event by default');
+
+    test.todo('sends a custom event');
+
+    test.todo('does not send an event with non-Algolia Insights hits');
+  });
+
+  describe('onActive', () => {
+    test.todo('does not send an event by default');
+
+    test.todo('sends a custom event');
+
+    test.todo('does not send an event with non-Algolia Insights hits');
+  });
 });
