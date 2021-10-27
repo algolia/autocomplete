e04f3027789864c8930d053f07e833067625cfaa
Sarah Dayan <5370675+sarahdayan@users.noreply.github.com>
2021-10-27 16:17:22 +0200

    docs(examples): move Vue example to Vite (#801)
    Co-authored-by: Danilo Woznica <danilowoz@gmail.com>

--- examples/vue/src/App.vue
+++ examples/vue/src/App.vue
@@ -4,15 +4,15 @@
   </div>
 </template>
 
-<script>
+<script lang="jsx">
 import { h, Fragment, render, onMounted } from 'vue';
 import { autocomplete, getAlgoliaResults } from '@algolia/autocomplete-js';
 import algoliasearch from 'algoliasearch/lite';
 
 import '@algolia/autocomplete-theme-classic';
 
-import { createElement } from './adapter';
-import ProductItem from './ProductItem.vue';
+import { createElement } from './utils/createElement';
+import ProductItem from './components/ProductItem.vue';
 
 const appId = 'latency';
 const apiKey = '6be0576ff61c053d5f9a3225e2a90f76';
