7e7b4fe2107c662146d05a9da584d956b2c8b2e2
Sarah Dayan <5370675+sarahdayan@users.noreply.github.com>
2021-11-26 22:16:05 +0100

    fix(createConcurrentSafePromise): better infer whether promises are running

--- packages/autocomplete-core/src/utils/createConcurrentSafePromise.ts
+++ packages/autocomplete-core/src/utils/createConcurrentSafePromise.ts
@@ -10,37 +10,41 @@ export function createConcurrentSafePromise() {
   let basePromiseId = -1;
   let latestResolvedId = -1;
   let latestResolvedValue: unknown = undefined;
+  let runningPromises = 0;
 
   function runConcurrentSafePromise<TValue>(promise: MaybePromise<TValue>) {
     basePromiseId++;
+    runningPromises++;
     const currentPromiseId = basePromiseId;
 
-    return Promise.resolve(promise).then((x) => {
-      // The promise might take too long to resolve and get outdated. This would
-      // result in resolving stale values.
-      // When this happens, we ignore the promise value and return the one
-      // coming from the latest resolved value.
-      //
-      // +----------------------------------+
-      // |        100ms                     |
-      // | run(1) +--->  R1                 |
-      // |        300ms                     |
-      // | run(2) +-------------> R2 (SKIP) |
-      // |        200ms                     |
-      // | run(3) +--------> R3             |
-      // +----------------------------------+
-      if (latestResolvedValue && currentPromiseId < latestResolvedId) {
-        return latestResolvedValue as TValue;
-      }
+    return Promise.resolve(promise)
+      .then((x) => {
+        // The promise might take too long to resolve and get outdated. This would
+        // result in resolving stale values.
+        // When this happens, we ignore the promise value and return the one
+        // coming from the latest resolved value.
+        //
+        // +----------------------------------+
+        // |        100ms                     |
+        // | run(1) +--->  R1                 |
+        // |        300ms                     |
+        // | run(2) +-------------> R2 (SKIP) |
+        // |        200ms                     |
+        // | run(3) +--------> R3             |
+        // +----------------------------------+
+        if (latestResolvedValue && currentPromiseId < latestResolvedId) {
+          return latestResolvedValue as TValue;
+        }
 
-      latestResolvedId = currentPromiseId;
-      latestResolvedValue = x;
+        latestResolvedId = currentPromiseId;
+        latestResolvedValue = x;
 
-      return x;
-    });
+        return x;
+      })
+      .finally(() => runningPromises--);
   }
 
-  runConcurrentSafePromise.isRunning = () => basePromiseId !== latestResolvedId;
+  runConcurrentSafePromise.isRunning = () => runningPromises > 0;
 
   return runConcurrentSafePromise;
 }
