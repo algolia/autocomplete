1adfc2777535edc6ad83db715e605b71673c1f9b
Sarah Dayan <5370675+sarahdayan@users.noreply.github.com>
2022-04-11 21:24:20 +0200

    feat: introduce pickBy utility

--- packages/autocomplete-js/src/utils/index.ts
+++ packages/autocomplete-js/src/utils/index.ts
@@ -1,4 +1,5 @@
 export * from './getHTMLElement';
 export * from './mergeClassNames';
 export * from './mergeDeep';
+export * from './pickBy';
 export * from './setProperties';
---
+++ packages/autocomplete-js/src/utils/pickBy.ts
@@ -0,0 +1,12 @@
+export function pickBy<TValue = unknown>(
+  obj: Record<string, TValue>,
+  predicate: (value: { key: string; value: TValue }) => boolean
+) {
+  return Object.entries(obj).reduce((acc, [key, value]) => {
+    if (predicate({ key, value })) {
+      return { ...acc, [key]: value };
+    }
+
+    return acc;
+  }, {});
+}
