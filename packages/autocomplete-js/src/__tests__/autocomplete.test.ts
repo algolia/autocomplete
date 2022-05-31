64589a79c976172d1c101f132f4c4579aa00c0f6
Mustafa Turhan <mustaphaturhan@gmail.com>
2022-05-31 16:48:29 +0300

    fix(js): update default test

--- packages/autocomplete-js/src/__tests__/autocomplete.test.ts
+++ packages/autocomplete-js/src/__tests__/autocomplete.test.ts
@@ -23,6 +23,7 @@ describe('autocomplete-js', () => {
     autocomplete<{ label: string }>({
       id: 'autocomplete',
       container,
+      useTrapFocus: false,
       getSources() {
         return [
           {
@@ -104,8 +105,8 @@ describe('autocomplete-js', () => {
                     stroke-dasharray="164.93361431346415 56.97787143782138"
                     stroke-width="6"
                   >
-                    
-        
+
+
                     <animatetransform
                       attributeName="transform"
                       dur="1s"
@@ -114,7 +115,7 @@ describe('autocomplete-js', () => {
                       type="rotate"
                       values="0 50 50;90 50 50;180 50 50;360 50 50"
                     />
-                    
+
 
                   </circle>
                 </svg>
