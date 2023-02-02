e8fad019c646015eebb64f063162a856183e84cf
Dan Rodriguez <daniel.rodriguez@algolia.com>
2023-02-02 07:31:49 -0500

    fix: missing icon

--- packages/autocomplete-plugin-redirect-url/src/templates.tsx
+++ packages/autocomplete-plugin-redirect-url/src/templates.tsx
@@ -7,17 +7,23 @@ export const defaultTemplates = {
       <div className="aa-ItemWrapper">
         <div className="aa-ItemContent">
           <div className="aa-ItemContentBody">
-            <div className="aa-ItemContentTitle">
-              {state.query}
-            </div>
+            <div className="aa-ItemContentTitle">{state.query}</div>
           </div>
         </div>
         <div className="aa-ItemActions">
-          <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-               stroke-linejoin="round">
-            <line x1="5" y1="12" x2="19" y2="12"/>
-            <polyline points="12 5 19 12 12 19"/>
-          </svg>
+          <div className="aa-ItemActionButton">
+            <svg
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <line x1="5" y1="12" x2="19" y2="12" />
+              <polyline points="12 5 19 12 12 19" />
+            </svg>
+          </div>
         </div>
       </div>
     );
