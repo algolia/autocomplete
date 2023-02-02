beb0b78f0bb2ed42b207e68bcc57a2b7e171850d
Dan Rodriguez <daniel.rodriguez@algolia.com>
2023-02-02 07:05:50 -0500

    chore: remove button from templates

--- packages/autocomplete-plugin-redirect-url/src/templates.tsx
+++ packages/autocomplete-plugin-redirect-url/src/templates.tsx
@@ -13,13 +13,11 @@ export const defaultTemplates = {
           </div>
         </div>
         <div className="aa-ItemActions">
-          <button className="aa-ItemActionButton">
-            <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                 stroke-linejoin="round">
-              <line x1="5" y1="12" x2="19" y2="12"/>
-              <polyline points="12 5 19 12 12 19"/>
-            </svg>
-          </button>
+          <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+               stroke-linejoin="round">
+            <line x1="5" y1="12" x2="19" y2="12"/>
+            <polyline points="12 5 19 12 12 19"/>
+          </svg>
         </div>
       </div>
     );
