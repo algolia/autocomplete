ff595a0f5c16d47fe42f0f22ef41dd360b15ad2d
Dhaya <154633+dhayab@users.noreply.github.com>
2023-04-05 11:24:17 +0200

    feat(preset-algolia): attach algolia credentials on hits

--- packages/autocomplete-plugin-algolia-insights/src/types/AlgoliaInsightsHit.ts
+++ packages/autocomplete-plugin-algolia-insights/src/types/AlgoliaInsightsHit.ts
@@ -2,4 +2,8 @@ export type AlgoliaInsightsHit = {
   objectID: string;
   __autocomplete_indexName: string;
   __autocomplete_queryID: string;
+  __autocomplete_algoliaResultsMetadata: {
+    appId: string;
+    apiKey: string;
+  };
 };
