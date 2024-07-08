76fbbb5f16c568f984c4b803e391008116d6bcb6
Dhaya <154633+dhayab@users.noreply.github.com>
2024-07-08 16:32:24 +0200

    fix(algoliasearch): correctly retrieve headers for v5

--- packages/autocomplete-preset-algolia/src/utils/__tests__/getAppIdAndApiKey.test.ts
+++ packages/autocomplete-preset-algolia/src/utils/__tests__/getAppIdAndApiKey.test.ts
@@ -1,4 +1,5 @@
 import algoliasearchV4 from 'algoliasearch';
+import { liteClient as algoliasearchV5 } from 'algoliasearch-v5/lite';
 
 import { getAppIdAndApiKey } from '../getAppIdAndApiKey';
 
@@ -6,8 +7,15 @@ const APP_ID = 'myAppId';
 const API_KEY = 'myApiKey';
 
 describe('getAppIdAndApiKey', () => {
-  it('gets appId and apiKey from searchClient', () => {
+  it('gets appId and apiKey from searchClient@v4', () => {
     const searchClient = algoliasearchV4(APP_ID, API_KEY);
+    const { appId, apiKey } = getAppIdAndApiKey(searchClient);
+    expect(appId).toEqual(APP_ID);
+    expect(apiKey).toEqual(API_KEY);
+  });
+
+  it('gets appId and apiKey from searchClient@v5', () => {
+    const searchClient = algoliasearchV5(APP_ID, API_KEY);
     const { appId, apiKey } = getAppIdAndApiKey(searchClient);
     expect(appId).toEqual(APP_ID);
     expect(apiKey).toEqual(API_KEY);
@@ -17,7 +25,6 @@ describe('getAppIdAndApiKey', () => {
     const searchClient = {
       search: algoliasearchV4(APP_ID, API_KEY).search,
     };
-    // @ts-expect-error
     const { appId, apiKey } = getAppIdAndApiKey(searchClient);
     expect(appId).toEqual(undefined);
     expect(apiKey).toEqual(undefined);
--- packages/autocomplete-preset-algolia/src/utils/getAppIdAndApiKey.ts
+++ packages/autocomplete-preset-algolia/src/utils/getAppIdAndApiKey.ts
@@ -1,10 +1,12 @@
-import type { SearchClient } from '../types';
-
-export function getAppIdAndApiKey(searchClient: SearchClient): {
+// typed as any, since it accepts the _real_ js clients, not the interface we otherwise expect
+export function getAppIdAndApiKey(searchClient: any): {
   appId: string;
   apiKey: string;
 } {
-  const { headers = {}, queryParameters = {} } = searchClient.transporter || {};
+  const transporter = searchClient.transporter || {};
+  const headers = transporter.headers || transporter.baseHeaders || {};
+  const queryParameters =
+    transporter.queryParameters || transporter.baseQueryParameters || {};
   const APP_ID = 'x-algolia-application-id';
   const API_KEY = 'x-algolia-api-key';
   const appId = headers[APP_ID] || queryParameters[APP_ID];
