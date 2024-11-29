0e70cd49edb119ec83ae4e5fb4b7570be3d10eb2
Shahmir Ejaz <shaejaz5@gmail.com>
2024-11-29 16:04:12 +0000

    fix tab spacing

--- packages/autocomplete-js/src/__tests__/autocomplete.test.ts
+++ packages/autocomplete-js/src/__tests__/autocomplete.test.ts
@@ -131,8 +131,8 @@ See: https://www.algolia.com/doc/ui-libraries/autocomplete/api-reference/autocom
                     stroke-dasharray="164.93361431346415 56.97787143782138"
                     stroke-width="6"
                   >
-
-
+                    
+        
                     <animatetransform
                       attributeName="transform"
                       dur="1s"
@@ -141,7 +141,7 @@ See: https://www.algolia.com/doc/ui-libraries/autocomplete/api-reference/autocom
                       type="rotate"
                       values="0 50 50;90 50 50;180 50 50;360 50 50"
                     />
-
+                    
 
                   </circle>
                 </svg>
