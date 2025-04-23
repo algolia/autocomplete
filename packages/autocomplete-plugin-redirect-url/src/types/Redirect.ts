b8c0cea70a1e7b1cbe817b039149ae12e8aa50d4
Dan Rodriguez <daniel.rodriguez@algolia.com>
2025-04-23 10:41:23 -0600

    feat: add new type and doc comments

--- packages/autocomplete-plugin-redirect-url/src/types/Redirect.ts
+++ packages/autocomplete-plugin-redirect-url/src/types/Redirect.ts
@@ -4,7 +4,10 @@ import {
   InternalAutocompleteOptions,
 } from '@algolia/autocomplete-core';
 import { SourceTemplates } from '@algolia/autocomplete-js';
-import { SearchForFacetValuesResponse } from '@algolia/autocomplete-preset-algolia';
+import {
+  SearchForFacetValuesResponse,
+  TransformedRequesterResponse,
+} from '@algolia/autocomplete-preset-algolia';
 import type { SearchResponse } from '@algolia/autocomplete-shared';
 
 export interface RedirectUrlPlugin {
@@ -24,18 +27,43 @@ export type OnRedirectOptions<TItem extends RedirectUrlItem> = {
   state: AutocompleteState<TItem>;
 };
 
-export type TransformResponseParams<TItem> =
+export interface TransformResponse {
+  url: string | undefined;
+  query: string | undefined;
+}
+
+export type Response<TItem> =
   | SearchResponse<TItem>
   | SearchForFacetValuesResponse;
 
 export type CreateRedirectUrlPluginParams<TItem extends BaseItem> = {
+  /**
+   * Map the response to values that can be interpreted by the plugin to correctly parse redirects.
+   *
+   * Supports Algolia results out of the box.
+   */
   transformResponse?(
-    response: TransformResponseParams<TItem>
-  ): string | undefined;
+    response: Response<TItem>
+  ): TransformResponse | string | undefined;
+  /**
+   * Handles the navigation logic once a redirect is triggered
+   *
+   * Supports Algolia results out of the box.
+   */
   onRedirect?(
     redirects: RedirectUrlItem[],
     options: OnRedirectOptions<RedirectUrlItem>
   ): void;
+  /**
+   * The template used to render injected redirect dropdown items.
+   */
   templates?: SourceTemplates<RedirectUrlItem>;
+  /**
+   * Waits for all pending requests to complete before handling a form submission .
+   * (ex: pressing the "enter" key in the input)
+   *
+   * A boolean return value will wait for all pending requests to resolve.
+   * A number value will achieve the above with a timeout (in ms) to exit if it takes too long.
+   */
   awaitSubmit?: () => boolean | number;
 };
